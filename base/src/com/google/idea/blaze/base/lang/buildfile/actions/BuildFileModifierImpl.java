9a35724fd099390b358d75dfd012a4ed80e2f3be
idank <idank@wix.com>
2018-12-26 05:06:12 +0200

    formatting the build file - rewritting the way target is added

--- base/src/com/google/idea/blaze/base/lang/buildfile/actions/BuildFileModifierImpl.java
+++ base/src/com/google/idea/blaze/base/lang/buildfile/actions/BuildFileModifierImpl.java
@@ -27,11 +27,18 @@ import com.google.idea.blaze.base.lang.buildfile.references.BuildReferenceManage
 import com.google.idea.blaze.base.lang.buildfile.search.BlazePackage;
 import com.google.idea.blaze.base.model.primitives.Kind;
 import com.google.idea.blaze.base.model.primitives.Label;
+import com.intellij.lang.ASTFactory;
+import com.intellij.lang.Language;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
+import com.intellij.psi.impl.GeneratedMarkerVisitor;
+import com.intellij.psi.impl.source.DummyHolderFactory;
+import com.intellij.psi.impl.source.tree.FileElement;
+import com.intellij.psi.impl.source.tree.LeafElement;
+import com.intellij.psi.tree.IElementType;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -79,10 +86,7 @@ public class BuildFileModifierImpl implements BuildFileModifier {
         if(targetToBeEdited.isPresent()){
             java.util.Optional<Argument.Keyword> maybeDepsKeyword = findTargetDepsAttribute(targetToBeEdited.get());
             if(!maybeDepsKeyword.isPresent()){
-                ArgumentList targetAttributes = PsiUtils.findFirstChildOfClassRecursive(targetToBeEdited.get(), ArgumentList.class);
-                addCommaToTargetAttributesIfNeeded(project, targetAttributes, ",\n\t\t");
-                addDepsAttributeToTargetAttributes(targetAttributes, project);
-                maybeDepsKeyword = findTargetDepsAttribute(targetToBeEdited.get());
+                maybeDepsKeyword = addDepsAttribute(project, targetToBeEdited.get());
             }
             addTargetToDepsIfNeeded(maybeDepsKeyword.get(), targetToBeAdded, project);
             wasDepsAddedToRule = true;
@@ -93,6 +97,20 @@ public class BuildFileModifierImpl implements BuildFileModifier {
   }
 
     @NotNull
+    private java.util.Optional<Argument.Keyword> addDepsAttribute(Project project, PsiElement targetToBeEdited) {
+        java.util.Optional<Argument.Keyword> maybeDepsKeyword;
+        ArgumentList targetAttributes = PsiUtils.findFirstChildOfClassRecursive(targetToBeEdited, ArgumentList.class);
+        addDepsAttributeToTargetAttributes(targetAttributes, project);
+        maybeDepsKeyword = findTargetDepsAttribute(targetToBeEdited);
+        PsiElement whiteSpaceLiteral = PsiParserFacade.SERVICE.getInstance(project).createWhiteSpaceFromText("\n\t");
+        targetAttributes.addBefore(whiteSpaceLiteral, maybeDepsKeyword.get());
+        PsiElement commaLiteral = createCommaElement(project);
+        targetAttributes.addAfter(commaLiteral, maybeDepsKeyword.get());
+
+        return maybeDepsKeyword;
+    }
+
+    @NotNull
     private java.util.Optional<Argument.Keyword> findTargetDepsAttribute(PsiElement psiElement) {
         List<Argument.Keyword> allChildKeywords = PsiUtils.findAllChildrenOfClassRecursive(psiElement, Argument.Keyword.class);
         Stream<Argument.Keyword> depsKeywords = allChildKeywords.stream().filter(keyword -> keyword.getName().equals("deps"));
@@ -100,38 +118,56 @@ public class BuildFileModifierImpl implements BuildFileModifier {
     }
 
     private void addTargetToDepsIfNeeded(Argument.Keyword depsKeyword, Label targetToBeAdded, Project project) {
-        PsiElement values = depsKeyword.getPsiChild(BuildElementTypes.LIST_LITERAL, null);
-        if(!doesDepsContainTarget(targetToBeAdded, values)){
-            addTargetToDeps(targetToBeAdded, project, values);
+        PsiElement depsElement = depsKeyword.getPsiChild(BuildElementTypes.LIST_LITERAL, null);
+        if(depsDoesntHaveWhiteSpace(depsElement)){
+            addWhiteSpaceElement(project, depsElement, "\n\t");
+        }
+        if(!doesDepsContainTarget(targetToBeAdded, depsElement)){
+            addTargetToDeps(targetToBeAdded, project, depsElement);
         }
     }
 
-    private void addTargetToDeps(Label targetToBeAdded, Project project, PsiElement values) {
+    private boolean depsDoesntHaveWhiteSpace(PsiElement depsElement) {
+        PsiWhiteSpace lastWhiteSpace = PsiUtils.findLastChildOfClassRecursive(depsElement, PsiWhiteSpace.class);
+        return lastWhiteSpace == null;
+    }
+
+    private void addTargetToDeps(Label targetToBeAdded, Project project, PsiElement depsElement) {
         PsiElementFactory factory = JavaPsiFacade.getInstance(project).getElementFactory();
-        addCommaToTargetAttributesIfNeeded(project, values, ", ");
-        addTargetToDeps(targetToBeAdded, values, factory);
-        addCommaToTargetAttributesIfNeeded(project, values, ",\n\t\t");
+        PsiLiteralExpression stringLiteral = (PsiLiteralExpression)factory.createExpressionFromText("\""+targetToBeAdded.toString()+"\"", null);
+        PsiWhiteSpace lastWhiteSpace = PsiUtils.findLastChildOfClassRecursive(depsElement, PsiWhiteSpace.class);
+        depsElement.addBefore(stringLiteral, lastWhiteSpace);
+
+        PsiElement commaLiteral = createCommaElement(project);
+        lastWhiteSpace = PsiUtils.findLastChildOfClassRecursive(depsElement, PsiWhiteSpace.class);
+        depsElement.addBefore(commaLiteral,lastWhiteSpace);
+
+        PsiLiteralExpression addedTarget = PsiUtils.findLastChildOfClassRecursive(depsElement, PsiLiteralExpression.class);
+        PsiElement whiteSpaceLiteral = PsiParserFacade.SERVICE.getInstance(project).createWhiteSpaceFromText("\n\t\t");
+        depsElement.addBefore(whiteSpaceLiteral, addedTarget);
     }
 
     private boolean doesDepsContainTarget(Label targetToBeAdded, PsiElement values) {
         return values.getText().contains(targetToBeAdded.toString());
     }
 
-    private void addDepsAttributeToTargetAttributes(PsiElement values, Project project) {
+    private void addDepsAttributeToTargetAttributes(PsiElement depsElement, Project project) {
         Argument.Keyword deps = BuildElementGenerator.getInstance(project).createKeywordArgumentWithListValue("deps");
-        values.addBefore(deps, values.getLastChild());
+        depsElement.addBefore(deps, depsElement.getLastChild().getPrevSibling());
     }
 
-    private void addTargetToDeps(Label targetToBeAdded, PsiElement values, PsiElementFactory factory) {
-        PsiLiteralExpression stringLiteral = (PsiLiteralExpression)factory.createExpressionFromText("\""+targetToBeAdded.toString()+"\"", null);
-        values.addBefore(stringLiteral, values.getLastChild());
+    private void addWhiteSpaceElement(Project project, PsiElement element, String whiteSpaceValue) {
+        PsiElement whiteSpaceLiteral = PsiParserFacade.SERVICE.getInstance(project).createWhiteSpaceFromText(whiteSpaceValue);
+        element.addBefore(whiteSpaceLiteral, element.getLastChild());
     }
 
-    private void addCommaToTargetAttributesIfNeeded(Project project, PsiElement attributes, String whiteSpaceValue) {
-      if(attributes.getChildren().length > 0){
-          PsiElement commaLiteral = PsiParserFacade.SERVICE.getInstance(project).createWhiteSpaceFromText(whiteSpaceValue);
-          attributes.addBefore(commaLiteral, attributes.getLastChild());
-      }
+    private PsiElement createCommaElement(Project project) {
+        PsiManager psiManager = PsiManager.getInstance(project);
+        final FileElement holderElement = DummyHolderFactory.createHolder(psiManager, null).getTreeElement();
+        final LeafElement newElement = ASTFactory.leaf(new IElementType("COMMA", Language.ANY), holderElement.getCharTable().intern(","));
+        holderElement.rawAddChildren(newElement);
+        GeneratedMarkerVisitor.markGenerated(newElement.getPsi());
+        return newElement.getPsi();
     }
 
     private PsiElement createRule(Project project, Kind ruleKind, String ruleName) {
