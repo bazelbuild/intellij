794df1187680e97f46b98b1c2338c7d9940af7de
Googler <intellij-github@google.com>
2019-03-18 08:34:07 -0700

    Update tsconfig prefetch to parse the tsconfig.json files from project view.
    PiperOrigin-RevId: 238990774

--- javascript/src/com/google/idea/blaze/typescript/TypeScriptPrefetchFileSource.java
+++ javascript/src/com/google/idea/blaze/typescript/TypeScriptPrefetchFileSource.java
@@ -15,36 +15,45 @@
  */
 package com.google.idea.blaze.typescript;
 
+import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.idea.blaze.base.ideinfo.ArtifactLocation;
 import com.google.idea.blaze.base.ideinfo.TargetIdeInfo;
+import com.google.idea.blaze.base.io.FileOperationProvider;
 import com.google.idea.blaze.base.model.BlazeProjectData;
+import com.google.idea.blaze.base.model.primitives.Label;
 import com.google.idea.blaze.base.model.primitives.LanguageClass;
 import com.google.idea.blaze.base.model.primitives.WorkspacePath;
-import com.google.idea.blaze.base.model.primitives.WorkspaceRoot;
 import com.google.idea.blaze.base.prefetch.PrefetchFileSource;
+import com.google.idea.blaze.base.projectview.ProjectViewManager;
 import com.google.idea.blaze.base.projectview.ProjectViewSet;
 import com.google.idea.blaze.base.sync.projectview.ImportRoots;
 import com.google.idea.common.experiments.BoolExperiment;
-import com.intellij.lang.typescript.tsconfig.TypeScriptConfigService;
-import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.util.io.FileUtilRt;
-import com.intellij.openapi.vfs.VfsUtil;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.function.Predicate;
 
 /** Declare that ts files should be prefetched. */
 public class TypeScriptPrefetchFileSource implements PrefetchFileSource {
+  private static final Logger logger = Logger.getInstance(TypeScriptPrefetchFileSource.class);
 
   private static final BoolExperiment prefetchAllTsSources =
       new BoolExperiment("prefetch.all.ts.sources", true);
   private static final BoolExperiment prefetchTsConfigFilesList =
-      new BoolExperiment("prefetch.tsconfig.files.list", true);
+      new BoolExperiment("prefetch.tsconfig.files.list2", true);
 
   @Override
   public void addFilesToPrefetch(
@@ -83,22 +92,57 @@ public class TypeScriptPrefetchFileSource implements PrefetchFileSource {
 
     if (prefetchTsConfigFilesList.getValue()) {
       // Prefetch all .d.ts files from the tsconfig
-      WorkspaceRoot root = WorkspaceRoot.fromProject(project);
-      Predicate<File> shouldPrefetch =
-          file -> {
-            if (!root.isInWorkspace(file)
-                || importRoots.containsWorkspacePath(root.workspacePathFor(file))) {
-              return false;
-            }
-            return prefetchFileExtensions().contains(FileUtilRt.getExtension(file.getPath()));
-          };
-      ReadAction.compute(() -> TypeScriptConfigService.Provider.getConfigFiles(project)).stream()
-          .map(config -> ReadAction.compute(config::getFileList))
-          .flatMap(Collection::stream)
-          .map(VfsUtil::virtualToIoFile)
-          .filter(shouldPrefetch)
-          .forEach(files::add);
+      files.addAll(getFilesFromTsConfigs(project, blazeProjectData));
     }
+  }
+
+  static Set<File> getFilesFromTsConfigs(Project project, BlazeProjectData projectData) {
+    ProjectViewSet projectViewSet = ProjectViewManager.getInstance(project).getProjectViewSet();
+    if (projectViewSet == null) {
+      return ImmutableSet.of();
+    }
+    File blazeBin = projectData.getBlazeInfo().getBlazeBinDirectory();
+    ImmutableSet.Builder<File> files = ImmutableSet.builder();
+    FileOperationProvider fOps = FileOperationProvider.getInstance();
+    for (Label label : getTsConfigTargets(projectViewSet)) {
+      File directory = new File(blazeBin, label.blazePackage().relativePath());
+      File tsconfig = new File(directory, "tsconfig_editor.json");
+      JsonObject json;
+      try {
+        json =
+            (JsonObject)
+                new JsonParser()
+                    .parse(new InputStreamReader(new FileInputStream(tsconfig), Charsets.UTF_8));
+      } catch (FileNotFoundException e) {
+        continue;
+      }
+      for (JsonElement fileElement : json.getAsJsonArray("files")) {
+        String relativePath = fileElement.getAsString();
+        if (relativePath.startsWith("..")) {
+          // these are in-source files, ignore them
+          continue;
+        }
+        files.add(maybeResolveSymlink(fOps, new File(directory, relativePath)));
+      }
+    }
+    return files.build();
+  }
+
+  private static File maybeResolveSymlink(FileOperationProvider fOps, File file) {
+    if (fOps.isSymbolicLink(file)) {
+      try {
+        return fOps.readSymbolicLink(file);
+      } catch (IOException e) {
+        logger.warn(e);
+      }
+    }
+    return file;
+  }
+
+  private static Set<Label> getTsConfigTargets(ProjectViewSet projectViewSet) {
+    Set<Label> labels = new LinkedHashSet<>(projectViewSet.listItems(TsConfigRulesSection.KEY));
+    projectViewSet.getScalarValue(TsConfigRuleSection.KEY).ifPresent(labels::add);
+    return labels;
   }
 
   @Override
