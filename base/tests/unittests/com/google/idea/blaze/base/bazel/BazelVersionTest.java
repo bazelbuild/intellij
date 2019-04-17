9803ec34759ff9636a88adb1b96150a0a4c6e51a
huanhuanchen <huanhuanchen@google.com>
2019-04-17 09:29:07 -0700

    Automatic GitHub Sync with Copybara
    PiperOrigin-RevId: 244010772

--- base/tests/unittests/com/google/idea/blaze/base/bazel/BazelVersionTest.java
+++ base/tests/unittests/com/google/idea/blaze/base/bazel/BazelVersionTest.java
@@ -21,7 +21,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** Unit tests for {@link BazelVersion}. */
+/** Unit tests for {@link BazelVersion}. test test */
 @RunWith(JUnit4.class)
 public class BazelVersionTest {
 
