22f03c8c93052f2ca8ac3be500f643f1febc4949
Jingwen Chen <jingwen@google.com>
2019-09-25 21:22:51 -0400

    Fix running main methods from any scala_library

--- scala/src/com/google/idea/blaze/scala/run/producers/GenerateDeployableJarTaskProvider.java
+++ scala/src/com/google/idea/blaze/scala/run/producers/GenerateDeployableJarTaskProvider.java
@@ -183,7 +183,8 @@ class GenerateDeployableJarTaskProvider
 
       List<File> outputs =
           BlazeArtifact.getLocalFiles(
-              buildResultHelper.getBuildArtifactsForTarget(target, file -> true));
+              buildResultHelper.getBuildArtifactsForTarget(
+                  target.withTargetName(target.targetName() + "_deploy.jar"), file -> true));
       if (outputs.isEmpty()) {
         throw new ExecutionException(
             String.format("Failed to find deployable jar when building %s", target));
