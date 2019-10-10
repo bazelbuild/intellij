bb953d070b93d92b3c175bb44599ad11d1a4324b
Googler <intellij-github@google.com>
2019-10-10 15:05:35 -0700

    Add more flexible BUILD file target name matching.
    kt_jvm_library currently produces java IDE info with target names
    plus a "_DO_NOT_DEPEND_JVM" at the end. This breaks BUILD file target
    name matching.
    
    This CL fixes issues like these by augmenting BuildFileUtils#findBuildTarget
    to look for a macro with matching prefix when the target isn't in the BUILD
    file.
    
    When a label is calculated, #findBuildTarget first checks to see if the
    label actually points to a target in the corresponding BUILD file.
    If it doesn't, then it iterates through the named macros in the BUILD
    file until it finds one with name matching the beginning of the label.
    
    The new macro prefix matching mechanism is enabled by the flag
    "buildfile.macro.prefix.matching.enabled"
    
    If all else fails, #findBuildTarget returns null.
    
    PiperOrigin-RevId: 274047598

--- base/src/com/google/idea/blaze/base/actions/BuildFileUtils.java
+++ base/src/com/google/idea/blaze/base/actions/BuildFileUtils.java
@@ -15,6 +15,12 @@
  */
 package com.google.idea.blaze.base.actions;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.idea.blaze.base.lang.buildfile.psi.BuildFile;
+import com.google.idea.blaze.base.lang.buildfile.psi.FuncallExpression;
+import com.google.idea.blaze.base.lang.buildfile.psi.LoadStatement;
 import com.google.idea.blaze.base.lang.buildfile.references.BuildReferenceManager;
 import com.google.idea.blaze.base.lang.buildfile.search.BlazePackage;
 import com.google.idea.blaze.base.model.BlazeProjectData;
@@ -22,17 +28,23 @@ import com.google.idea.blaze.base.model.primitives.Label;
 import com.google.idea.blaze.base.model.primitives.WorkspacePath;
 import com.google.idea.blaze.base.sync.data.BlazeProjectDataManager;
 import com.google.idea.blaze.base.targetmaps.SourceToTargetMap;
+import com.google.idea.common.experiments.BoolExperiment;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFileSystemItem;
 import com.intellij.psi.PsiManager;
 import java.io.File;
+import java.util.Arrays;
+import java.util.Set;
 import javax.annotation.Nullable;
 
 /** BUILD-file utility methods used by actions. */
 final class BuildFileUtils {
   private BuildFileUtils() {}
+
+  static final BoolExperiment enableMacroPrefixMatching =
+      new BoolExperiment("buildfile.macro.prefix.matching.enabled", true);
 
   @Nullable
   static BlazePackage getBuildFile(Project project, @Nullable VirtualFile vf) {
@@ -63,15 +75,54 @@ final class BuildFileUtils {
       return null;
     }
     Label label =
-        SourceToTargetMap.getInstance(project)
-            .getTargetsToBuildForSourceFile(file)
-            .stream()
+        SourceToTargetMap.getInstance(project).getTargetsToBuildForSourceFile(file).stream()
             .filter(l -> l.blazePackage().equals(packagePath))
             .findFirst()
             .orElse(null);
     if (label == null) {
       return null;
     }
-    return BuildReferenceManager.getInstance(project).resolveLabel(label);
+
+    PsiElement targetElement = BuildReferenceManager.getInstance(project).resolveLabel(label);
+    if (targetElement != null) {
+      return targetElement;
+    }
+
+    if (enableMacroPrefixMatching.getValue()) {
+      Label macroWithMatchingPrefix = findMacroWithMatchingPrefix(parentPackage.buildFile, label);
+      if (macroWithMatchingPrefix != null) {
+        return BuildReferenceManager.getInstance(project).resolveLabel(macroWithMatchingPrefix);
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Returns the label of a macro with name prefixing the target name of a given label with a '_' or
+   * '-' delimiter.
+   */
+  @Nullable
+  @VisibleForTesting
+  static Label findMacroWithMatchingPrefix(BuildFile buildFile, Label label) {
+    Set<String> loadedSymbols =
+        Arrays.stream(buildFile.findChildrenByClass(LoadStatement.class))
+            .flatMap(l -> Arrays.stream(l.getVisibleSymbolNames()))
+            .collect(toImmutableSet());
+
+    String nameToMatch = label.targetName().toString();
+    for (FuncallExpression expr : buildFile.findChildrenByClass(FuncallExpression.class)) {
+      String name = expr.getNameArgumentValue();
+      if (loadedSymbols.contains(expr.getFunctionName())
+          && name != null
+          && name.length() < nameToMatch.length()
+          && nameToMatch.startsWith(name)
+          && (nameToMatch.charAt(name.length()) == '_'
+              || nameToMatch.charAt(name.length()) == '-')) {
+        return label.withTargetName(name);
+      }
+    }
+
+    return null;
   }
 }
