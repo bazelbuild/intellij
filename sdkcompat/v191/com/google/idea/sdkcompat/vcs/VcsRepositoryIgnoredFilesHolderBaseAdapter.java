52fb1f336adf20e556c17aede041c4c54974fa46
Googler <intellij-github@google.com>
2019-12-04 12:55:21 -0800

    compat: Compatibility shims for VCS API
    PiperOrigin-RevId: 283815642

---
+++ sdkcompat/v191/com/google/idea/sdkcompat/vcs/VcsRepositoryIgnoredFilesHolderBaseAdapter.java
@@ -0,0 +1,17 @@
+package com.google.idea.sdkcompat.vcs;
+
+import com.intellij.dvcs.ignore.VcsRepositoryIgnoredFilesHolderBase;
+import com.intellij.dvcs.repo.AbstractRepositoryManager;
+import com.intellij.dvcs.repo.Repository;
+
+/** #api191: VcsRepositoryIgnoredFilesHolderBase compat for 2019.1. */
+public abstract class VcsRepositoryIgnoredFilesHolderBaseAdapter<RepositoryT extends Repository>
+    extends VcsRepositoryIgnoredFilesHolderBase<RepositoryT> {
+  public VcsRepositoryIgnoredFilesHolderBaseAdapter(
+      RepositoryT repository,
+      AbstractRepositoryManager<RepositoryT> repositoryManager,
+      String updateQueueName,
+      String rescanIdentityName) {
+    super(repository, repositoryManager, updateQueueName, rescanIdentityName);
+  }
+}
