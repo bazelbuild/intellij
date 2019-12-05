cac81e024847b2e8c34146d6599a324399923fb3
Googler <intellij-github@google.com>
2019-12-04 16:32:18 -0800

    compat: VcsVFSListener changes to do with deleting files
    PiperOrigin-RevId: 283862924

---
+++ sdkcompat/v191/com/google/idea/sdkcompat/vcs/VcsVFSListenerAdapter.java
@@ -0,0 +1,36 @@
+package com.google.idea.sdkcompat.vcs;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.AbstractVcs;
+import com.intellij.openapi.vcs.FilePath;
+import com.intellij.openapi.vcs.VcsVFSListener;
+import java.util.List;
+
+/** #api192: VcsVFSListener compatibility bridge */
+public abstract class VcsVFSListenerAdapter extends VcsVFSListener {
+
+  protected VcsVFSListenerAdapter(AbstractVcs<?> vcs) {
+    super(vcs.getProject(), vcs);
+  }
+
+  /** #api192: remove */
+  protected VcsVFSListenerAdapter(Project project, AbstractVcs<?> vcs) {
+    super(project, vcs);
+  }
+
+  /** #api192: move to caller */
+  protected AllDeletedFiles acquireAllDeletedFiles() {
+    return new AllDeletedFiles(myDeletedFiles, myDeletedWithoutConfirmFiles);
+  }
+
+  /** Similar to VcsVFSListener.AllDeletedFiles in 2019.3 */
+  protected static class AllDeletedFiles {
+    public final List<FilePath> deletedFiles;
+    public final List<FilePath> deletedWithoutConfirmFiles;
+
+    AllDeletedFiles(List<FilePath> deletedFiles, List<FilePath> deletedWithoutConfirmFiles) {
+      this.deletedFiles = deletedFiles;
+      this.deletedWithoutConfirmFiles = deletedWithoutConfirmFiles;
+    }
+  }
+}
---
+++ sdkcompat/v193/com/google/idea/sdkcompat/vcs/VcsVFSListenerAdapter.java
@@ -0,0 +1,23 @@
+package com.google.idea.sdkcompat.vcs;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.AbstractVcs;
+import com.intellij.openapi.vcs.VcsVFSListener;
+
+/** #api192: VcsVFSListener compatibility bridge */
+public abstract class VcsVFSListenerAdapter extends VcsVFSListener {
+
+  protected VcsVFSListenerAdapter(AbstractVcs vcs) {
+    super(vcs);
+  }
+
+  /** #api192: remove */
+  protected VcsVFSListenerAdapter(Project project, AbstractVcs vcs) {
+    super(vcs);
+  }
+
+  /** #api192: move to caller */
+  public AllDeletedFiles acquireAllDeletedFiles() {
+    return this.myProcessor.acquireAllDeletedFiles();
+  }
+}
