87dd998162f59c96c43a984d4522e7ba5fb021ce
Googler <intellij-github@google.com>
2020-02-20 13:41:43 -0800

    sdkcompat simplifications after dropping support for #api191
    PiperOrigin-RevId: 296286667

--- java/src/com/google/idea/blaze/java/run/coverage/BlazeCoverageProjectViewClassDecorator.java
+++ java/src/com/google/idea/blaze/java/run/coverage/BlazeCoverageProjectViewClassDecorator.java
@@ -15,7 +15,7 @@
  */
 package com.google.idea.blaze.java.run.coverage;
 
-import com.google.idea.sdkcompat.openapi.AbstractCoverageProjectViewNodeDecoratorCompat;
+import com.intellij.coverage.AbstractCoverageProjectViewNodeDecorator;
 import com.intellij.coverage.CoverageAnnotator;
 import com.intellij.coverage.CoverageDataManager;
 import com.intellij.coverage.CoverageSuitesBundle;
@@ -36,10 +36,10 @@ import javax.annotation.Nullable;
  * here because the default decorator only handles PsiFile elements.
  */
 public class BlazeCoverageProjectViewClassDecorator
-    extends AbstractCoverageProjectViewNodeDecoratorCompat {
+    extends AbstractCoverageProjectViewNodeDecorator {
 
   BlazeCoverageProjectViewClassDecorator(Project project) {
-    super(project, CoverageDataManager.getInstance(project));
+    super(project);
   }
 
   @Override
