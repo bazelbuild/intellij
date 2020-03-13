9d84e6d2632e4b0340d8f8f6d78b23453aa314c8
Googler <intellij-github@google.com>
2020-03-13 12:07:10 -0700

    Import Android Studio 4.1 Canary 2 (4.1.0.2)
    PiperOrigin-RevId: 300801683

--- WORKSPACE
+++ WORKSPACE
@@ -243,8 +243,8 @@ http_archive(
 http_archive(
     name = "android_studio_4_1",
     build_file = "@//intellij_platform_sdk:BUILD.android_studio41",
-    sha256 = "1deb9157d8a9b21053fb50031a0b307dd2cd1b460caa923f794ca2de8f733a56",
-    url = "https://dl.google.com/dl/android/studio/ide-zips/4.1.0.1/android-studio-ide-193.6224510-linux.tar.gz",
+    sha256 = "7f1aac83023c1768be0fe894c18ea86d7662dae8f44a8da94c71147325c10954",
+    url = "https://dl.google.com/dl/android/studio/ide-zips/4.1.0.2/android-studio-ide-193.6264773-linux.tar.gz",
 )
 
 # LICENSE: Common Public License 1.0
--- aswb/tests/integrationtests/com/google/idea/blaze/android/functional/BlazeApkBuildStepMobileInstallIntegrationTest.java
+++ aswb/tests/integrationtests/com/google/idea/blaze/android/functional/BlazeApkBuildStepMobileInstallIntegrationTest.java
@@ -55,11 +55,13 @@ import com.google.idea.blaze.base.scope.BlazeContext;
 import com.google.idea.blaze.base.scope.output.IssueOutput;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.SimpleColoredComponent;
+import com.intellij.util.Function;
 import java.io.File;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Before;
 import org.junit.Test;
@@ -387,11 +389,21 @@ public class BlazeApkBuildStepMobileInstallIntegrationTest extends BlazeAndroidI
     @Override
     public void prepareToRenderLabel() {}
 
-    @Override
+    // api40: see new API for canRun below
     public LaunchCompatibility canRun(
         com.android.sdklib.AndroidVersion androidVersion,
         IAndroidTarget iAndroidTarget,
         EnumSet<HardwareFeature> enumSet,
+        @Nullable Set<String> set) {
+      return null;
+    }
+
+    // @Override #api40
+    public LaunchCompatibility canRun(
+        com.android.sdklib.AndroidVersion androidVersion,
+        IAndroidTarget iAndroidTarget,
+        AndroidFacet facet,
+        Function<AndroidFacet, EnumSet<HardwareFeature>> getRequiredHardwareFeatures,
         @Nullable Set<String> set) {
       return null;
     }
