27080931973cc6e6768bdac48bd5ac116afad81f
Googler <intellij-github@google.com>
2020-05-29 16:31:31 -0700

    Branch sdkcompat/as41 -> sdkcompat/as42
    PiperOrigin-RevId: 313871428

---
+++ aswb/sdkcompat/as42/com/google/idea/blaze/android/run/binary/mobileinstall/BlazeAndroidBinaryMobileInstallRunContext.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2016 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.idea.blaze.android.run.binary.mobileinstall;
+
+import com.android.tools.idea.run.ApkProvisionException;
+import com.android.tools.idea.run.LaunchOptions;
+import com.android.tools.idea.run.activity.DefaultStartActivityFlagsProvider;
+import com.android.tools.idea.run.activity.StartActivityFlagsProvider;
+import com.android.tools.idea.run.editor.AndroidDebugger;
+import com.android.tools.idea.run.editor.AndroidDebuggerState;
+import com.android.tools.idea.run.editor.ProfilerState;
+import com.android.tools.idea.run.tasks.DebugConnectorTask;
+import com.android.tools.idea.run.tasks.LaunchTask;
+import com.android.tools.idea.run.tasks.LaunchTasksProvider;
+import com.android.tools.idea.run.util.ProcessHandlerLaunchStatus;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.idea.blaze.android.run.binary.BlazeAndroidBinaryApplicationLaunchTaskProvider;
+import com.google.idea.blaze.android.run.binary.BlazeAndroidBinaryRunConfigurationState;
+import com.google.idea.blaze.android.run.binary.UserIdHelper;
+import com.google.idea.blaze.android.run.deployinfo.BlazeAndroidDeployInfo;
+import com.google.idea.blaze.android.run.runner.BlazeAndroidLaunchTasksProvider;
+import com.google.idea.blaze.android.run.runner.BlazeAndroidRunConfigurationDebuggerManager;
+import com.google.idea.blaze.base.model.primitives.Label;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.openapi.project.Project;
+import java.util.Set;
+import javax.annotation.Nullable;
+import org.jetbrains.android.facet.AndroidFacet;
+
+/** Run Context for mobile install launches, #api3.5 compat. */
+public class BlazeAndroidBinaryMobileInstallRunContext
+    extends BlazeAndroidBinaryMobileInstallRunContextBase {
+  public BlazeAndroidBinaryMobileInstallRunContext(
+      Project project,
+      AndroidFacet facet,
+      RunConfiguration runConfiguration,
+      ExecutionEnvironment env,
+      BlazeAndroidBinaryRunConfigurationState configState,
+      Label label,
+      ImmutableList<String> blazeFlags,
+      ImmutableList<String> exeFlags) {
+    super(project, facet, runConfiguration, env, configState, label, blazeFlags, exeFlags);
+  }
+
+  @Override
+  public void augmentLaunchOptions(LaunchOptions.Builder options) {
+    super.augmentLaunchOptions(options);
+    options.addExtraOptions(
+        ImmutableMap.of(ProfilerState.ANDROID_PROFILER_STATE_ID, configState.getProfilerState()));
+  }
+
+  @Override
+  public LaunchTasksProvider getLaunchTasksProvider(
+      LaunchOptions.Builder launchOptionsBuilder,
+      boolean isDebug,
+      BlazeAndroidRunConfigurationDebuggerManager debuggerManager)
+      throws ExecutionException {
+    return new BlazeAndroidLaunchTasksProvider(
+        project, this, applicationIdProvider, launchOptionsBuilder, isDebug, debuggerManager);
+  }
+
+  @Override
+  public LaunchTask getApplicationLaunchTask(
+      LaunchOptions launchOptions,
+      @Nullable Integer userId,
+      String contributorsAmStartOptions,
+      AndroidDebugger androidDebugger,
+      AndroidDebuggerState androidDebuggerState,
+      ProcessHandlerLaunchStatus processHandlerLaunchStatus)
+      throws ExecutionException {
+
+    String extraFlags = UserIdHelper.getFlagsFromUserId(userId);
+    if (!contributorsAmStartOptions.isEmpty()) {
+      extraFlags += (extraFlags.isEmpty() ? "" : " ") + contributorsAmStartOptions;
+    }
+
+    final StartActivityFlagsProvider startActivityFlagsProvider =
+        new DefaultStartActivityFlagsProvider(
+            androidDebugger, androidDebuggerState, project, launchOptions.isDebug(), extraFlags);
+    BlazeAndroidDeployInfo deployInfo;
+    try {
+      deployInfo = buildStep.getDeployInfo();
+    } catch (ApkProvisionException e) {
+      throw new ExecutionException(e);
+    }
+
+    return BlazeAndroidBinaryApplicationLaunchTaskProvider.getApplicationLaunchTask(
+        applicationIdProvider,
+        deployInfo.getMergedManifest(),
+        configState,
+        startActivityFlagsProvider,
+        processHandlerLaunchStatus);
+  }
+
+  @Nullable
+  @Override
+  @SuppressWarnings("unchecked")
+  public DebugConnectorTask getDebuggerTask(
+      AndroidDebugger androidDebugger,
+      AndroidDebuggerState androidDebuggerState,
+      Set<String> packageIds)
+      throws ExecutionException {
+    return androidDebugger.getConnectDebuggerTask(
+        env,
+        null,
+        packageIds,
+        facet,
+        androidDebuggerState,
+        runConfiguration.getType().getId(),
+        null);
+  }
+}
