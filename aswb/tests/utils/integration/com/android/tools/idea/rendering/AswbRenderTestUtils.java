eb9a03f44d2fc95659b74c85d4132653e220a1ef
Googler <intellij-github@google.com>
2021-01-11 12:26:52 -0800

    Check both internal and external SDK paths when initializing layoutlib
    Currently AswbRenderTestUtils#getStudioSdkPath only checks the internal
    repository path for the SDK and copies the required files if found. This
    fails when the test is run from external github repository because the
    SDK location is different.
    
    This CL fixes this by checking both internal and external paths when
    initializing the SDK to ensure the required files can be found in both
    scenarios.
    
    PiperOrigin-RevId: 351210891

--- aswb/tests/utils/integration/com/android/tools/idea/rendering/AswbRenderTestUtils.java
+++ aswb/tests/utils/integration/com/android/tools/idea/rendering/AswbRenderTestUtils.java
@@ -29,6 +29,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Utils for ASwB's render tests. This class is packaged under "com.android.tools.idea.rendering"
@@ -44,11 +46,17 @@ public class AswbRenderTestUtils {
    * Returns the workspace path to the android studio SDK used by this test which can change
    * depending on the SDK used for testing.
    */
-  private static String getStudioSdkPath() {
+  private static Optional<String> getStudioSdkPath() {
     ApplicationInfo appInfo = ApplicationInfo.getInstance();
-    return String.format(
-        "./third_party/java/jetbrains/plugin_api/android_studio_%s_%s/",
-        appInfo.getMajorVersion(), appInfo.getMinorVersion());
+    return Stream.of(
+            "./third_party/java/jetbrains/plugin_api/android_studio_%s_%s/",
+            // Matches path of SDK in github/bazelbuild/intellij.
+            "./external/android_studio_%s_%s/android-studio/")
+        .map(
+            (template) ->
+                String.format(template, appInfo.getMajorVersion(), appInfo.getMinorVersion()))
+        .filter((path) -> Paths.get(path).toAbsolutePath().toFile().exists())
+        .findFirst();
   }
 
   /**
@@ -59,10 +67,13 @@ public class AswbRenderTestUtils {
     // locations. All of those locations are sub directories of android studio's installation
     // directory and therefore can't be used directly in this test environment.  Therefore we need
     // to symlink the resources folder to a location studio will find.
-    Path srcFolder = Paths.get(getStudioSdkPath(), LAYOUTLIB_SRC_PATH).toAbsolutePath();
-    Path destFolder = Paths.get(PathManager.getHomePath(), LAYOUTLIB_SRC_PATH);
-    Files.createDirectories(destFolder.getParent());
-    Files.createSymbolicLink(destFolder, srcFolder);
+    Optional<String> externalStudioSdkPath = getStudioSdkPath();
+    if (externalStudioSdkPath.isPresent()) {
+      Path srcFolder = Paths.get(externalStudioSdkPath.get(), LAYOUTLIB_SRC_PATH).toAbsolutePath();
+      Path destFolder = Paths.get(PathManager.getHomePath(), LAYOUTLIB_SRC_PATH);
+      Files.createDirectories(destFolder.getParent());
+      Files.createSymbolicLink(destFolder, srcFolder);
+    }
 
     // Give the render executor 5 seconds to shutdown.
     RenderService.shutdownRenderExecutor(5);
