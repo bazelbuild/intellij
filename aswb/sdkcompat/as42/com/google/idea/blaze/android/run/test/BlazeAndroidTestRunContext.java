03bdfaf121fbe3ff37391e4f80592280753d564f
Googler <intellij-github@google.com>
2021-05-12 15:45:09 -0700

    Delete AndroidDebuggerCompat
    This is obsolete after we removed support for AS 4.1.
    
    PiperOrigin-RevId: 373467677

--- aswb/sdkcompat/as42/com/google/idea/blaze/android/run/test/BlazeAndroidTestRunContext.java
+++ aswb/sdkcompat/as42/com/google/idea/blaze/android/run/test/BlazeAndroidTestRunContext.java
@@ -20,7 +20,6 @@ import static com.google.idea.blaze.android.run.binary.BlazeAndroidBinaryNormalB
 import com.android.ddmlib.IDevice;
 import com.android.tools.idea.run.LaunchOptions;
 import com.android.tools.idea.run.editor.AndroidDebugger;
-import com.android.tools.idea.run.editor.AndroidDebuggerCompat;
 import com.android.tools.idea.run.editor.AndroidDebuggerState;
 import com.android.tools.idea.run.tasks.DebugConnectorTask;
 import com.android.tools.idea.run.tasks.LaunchTask;
@@ -86,14 +85,14 @@ public class BlazeAndroidTestRunContext extends BlazeAndroidTestRunContextBase {
             env.getProject(), androidDebugger, packageIds, applicationIdProvider, this);
       case NON_BLAZE:
       case MOBILE_INSTALL:
-        return AndroidDebuggerCompat.getConnectDebuggerTask(
-            androidDebugger,
+        return androidDebugger.getConnectDebuggerTask(
             env,
             null,
             packageIds,
             facet,
             androidDebuggerState,
-            runConfiguration.getType().getId());
+            runConfiguration.getType().getId(),
+            null);
     }
     throw new AssertionError();
   }
