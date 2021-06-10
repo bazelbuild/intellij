16d68674b8b0191662552ad937954f5ad6937309
Googler <intellij-github@google.com>
2021-06-10 09:22:02 -0700

    CLion 2020.1 compatibility
    PiperOrigin-RevId: 378666515

--- sdkcompat/v203/com/google/idea/sdkcompat/clion/CidrLauncherAdapter.java
+++
@@ -1,6 +0,0 @@
-package com.google.idea.sdkcompat.clion;
-
-import com.jetbrains.cidr.execution.CidrLauncher;
-
-/** Api compat with 2020.2 #api201 */
-public abstract class CidrLauncherAdapter extends CidrLauncher {}
