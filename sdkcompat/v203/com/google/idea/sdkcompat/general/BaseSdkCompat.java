3a2454e903df8b3a98798638b9f46f016dbec52c
Googler <intellij-github@google.com>
2021-06-11 04:31:06 -0700

    Remove obsolete methods for 201 compatibility from BaseSdkCompat
    PiperOrigin-RevId: 378848126

--- sdkcompat/v203/com/google/idea/sdkcompat/general/BaseSdkCompat.java
+++ sdkcompat/v203/com/google/idea/sdkcompat/general/BaseSdkCompat.java
@@ -1,85 +1,31 @@
 package com.google.idea.sdkcompat.general;
 
-import com.intellij.codeInsight.daemon.LineMarkerInfo;
-import com.intellij.codeInsight.daemon.LineMarkerProvider;
 import com.intellij.conversion.ConversionContext;
 import com.intellij.diagnostic.VMOptions;
-import com.intellij.diff.DiffContentFactoryImpl;
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereManager;
-import com.intellij.ide.impl.OpenProjectTask;
-import com.intellij.ide.plugins.PluginManagerCore;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.ex.util.EditorFacade;
-import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ex.ProjectManagerEx;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.changes.ui.ChangesListView;
 import com.intellij.psi.PsiElement;
-import com.intellij.ui.content.ContentManager;
 import com.intellij.usageView.UsageTreeColors;
 import com.intellij.usageView.UsageTreeColorsScheme;
 import com.intellij.usages.TextChunk;
-import com.intellij.util.ContentUtilEx;
 import com.jetbrains.python.psi.LanguageLevel;
-import java.nio.charset.Charset;
 import java.nio.file.Path;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 import javax.annotation.Nullable;
-import javax.swing.JComponent;
 
 /** Provides SDK compatibility shims for base plugin API classes, available to all IDEs. */
 public final class BaseSdkCompat {
   private BaseSdkCompat() {}
-
-  /** #api201: wildcard generics added in 2020.2. */
-  public interface LineMarkerProviderAdapter extends LineMarkerProvider {
-    @Override
-    default void collectSlowLineMarkers(
-        List<? extends PsiElement> elements, Collection<? super LineMarkerInfo<?>> result) {
-      doCollectSlowLineMarkers(elements, result);
-    }
-
-    void doCollectSlowLineMarkers(
-        List<? extends PsiElement> elements, Collection<? super LineMarkerInfo<?>> result);
-  }
-
-  /** #api201: project opening API changed in 2020.2. */
-  public static void openProject(Project project, Path projectFile) {
-    ProjectManagerEx.getInstanceEx()
-        .openProject(projectFile, OpenProjectTask.withCreatedProject(project));
-  }
-
-  /** #api201: changed in 2020.2 */
-  public static boolean isDisabledPlugin(PluginId id) {
-    return PluginManagerCore.isDisabled(id);
-  }
-
-  /** #api201: changed in 2020.2 */
-  public static Charset guessCharsetFromVcsRevisionData(
-      Project project, byte[] revisionContent, FilePath filePath) {
-    return DiffContentFactoryImpl.guessCharset(project, revisionContent, filePath);
-  }
-
-  /** #api201: ContentUtilEx changed in 2020.2 */
-  public static void addTabbedContent(
-      ContentManager manager,
-      JComponent contentComponent,
-      String groupPrefix,
-      String tabName,
-      boolean select,
-      @Nullable Disposable childDisposable) {
-    ContentUtilEx.addTabbedContent(
-        manager, contentComponent, groupPrefix, tabName, select, childDisposable);
-  }
 
   // #api202: Method return type changed in 2020.3 from File to Path
   @Nullable
