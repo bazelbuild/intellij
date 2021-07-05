ee744dd1f58f868aeaa470ab16a0c03476751a55
Mai Hussien <messa@google.com>
2021-07-05 02:39:02 -0700

    Add 2021.2 sdkcompat folder
    PiperOrigin-RevId: 383097668

---
+++ sdkcompat/v212/com/google/idea/sdkcompat/python/PyParserAdapter.java
@@ -0,0 +1,6 @@
+package com.google.idea.sdkcompat.python;
+
+import com.jetbrains.python.parsing.PyParser;
+
+/** Compatibility adapter for {@link PyParser}. #api202 */
+public abstract class PyParserAdapter extends PyParser {}
