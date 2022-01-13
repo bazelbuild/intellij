f2c5624c514bc3146a4575b9959def7e9efaf909
Googler <intellij-github@google.com>
2022-01-13 09:07:36 -0800

    Fix package name declaration.
    PiperOrigin-RevId: 421580547

--- testing/testcompat/v212/com/google/idea/testing/cidr/BaseClionSdkTestCompat.java
+++ testing/testcompat/v212/com/google/idea/testing/cidr/BaseClionSdkTestCompat.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.idea.sdkcompat;
+package com.google.idea.testing.cidr;
 
 import com.intellij.openapi.project.Project;
 import com.jetbrains.cidr.lang.symbols.symtable.building.OCSymbolTablesBuildingActivity;
