d5f4adb72dd1a615e04372d052422b7adf2d3200
fedor <fedor.korotkov@gmail.com>
2022-02-09 07:42:04 -0500

    Disable invalidation by default

--- base/src/com/google/idea/blaze/base/sync/data/BlazeProjectDataInvalidator.java
+++ base/src/com/google/idea/blaze/base/sync/data/BlazeProjectDataInvalidator.java
@@ -3,6 +3,8 @@ package com.google.idea.blaze.base.sync.data;
 import com.google.idea.blaze.base.io.FileOperationProvider;
 import com.intellij.ide.caches.CachesInvalidator;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.NlsContexts;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,6 +12,21 @@ import java.io.IOException;
 public class BlazeProjectDataInvalidator extends CachesInvalidator {
   private static final Logger logger =
       Logger.getInstance(BlazeProjectDataInvalidator.class.getName());
+
+  @Override
+  public @Nullable @NlsContexts.Checkbox String getDescription() {
+    return "Clear all Bazel caches";
+  }
+
+  @Override
+  public @Nullable @NlsContexts.DetailedDescription String getComment() {
+    return "Invalidates all IDE related data for all Bazel projects";
+  }
+
+  @Override
+  public @Nullable Boolean optionalCheckboxDefaultValue() {
+    return false;
+  }
 
   @Override
   public void invalidateCaches() {
