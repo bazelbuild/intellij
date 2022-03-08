ece2950ae797d0987cb7e2bff72ec97a4ed6c987
Googler <intellij-github@google.com>
2022-03-08 13:35:40 -0800

    Remove "(NEW)" from the headings of Blaze Tasks and Blaze Problems windows
    The new Blaze console UI has toolwindows with headings such as "Blaze Tasks (NEW)" and "Blaze Tasks Problems (NEW)". This CL removes the label "(NEW)" from such headings.
    
    PiperOrigin-RevId: 433297815

--- base/src/com/google/idea/blaze/base/toolwindow/TasksToolWindowFactory.java
+++ base/src/com/google/idea/blaze/base/toolwindow/TasksToolWindowFactory.java
@@ -33,7 +33,7 @@ public class TasksToolWindowFactory implements DumbAware, ToolWindowFactory {
 
   @Override
   public void createToolWindowContent(Project project, ToolWindow toolWindow) {
-    String title = "Build Tasks (NEW)"; // TODO(olegsa) remove "(NEW)", and find some better name
+    String title = "Build Tasks";
     toolWindow.setTitle(title);
     toolWindow.setStripeTitle(title);
   }
