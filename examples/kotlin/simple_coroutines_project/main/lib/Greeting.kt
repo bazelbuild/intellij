007e92f6a0ff5fa3d914660659b26a11c26a3fe4
Mai Hussien <messa@google.com>
2022-05-03 12:02:28 -0700

    Update Kotlin coroutines example
    Update classes that contain only a companion object to be [named object declaration](https://kotlinlang.org/docs/object-declarations.html#object-declarations-overview) instead.
    
    PiperOrigin-RevId: 446255593

--- examples/kotlin/simple_coroutines_project/main/lib/Greeting.kt
+++ examples/kotlin/simple_coroutines_project/main/lib/Greeting.kt
@@ -2,11 +2,9 @@ package main.lib
 
 import kotlinx.coroutines.delay
 
-class Greeting {
-  companion object {
-    suspend fun getGreeting(id: Int): String {
-      delay(1000L * id)
-      return "Hello from Coroutine $id"
-    }
+object Greeting {
+  suspend fun getGreeting(id: Int): String {
+    delay(1000L * id)
+    return "Hello from Coroutine $id"
   }
 }
