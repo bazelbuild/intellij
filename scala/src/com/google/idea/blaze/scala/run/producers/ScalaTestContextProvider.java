4e9b51696ba1c722dcb55d1aea2f269f82129de4
Stig Rohde Døssing <stig.doessing@crowdstrike.com>
2022-06-28 14:44:46 +0200

    Allow running single Scalatest tests in Intellij.
    ScalaTestContextProvider is responsible for figuring out which parameters
    to pass to the Scalatest runner. It contains code capable of deciding which
    test class to run, but not which individual test case in the class to run.
    
    This commit replaces the Bazel plugin's own logic for this with a call to
    the corresponding logic in the Scala plugin, which is capable of deciding
    both the class and test name.
    
    When no test name is detected (e.g. due to running a whole class), the
    plugin will continue passing the class name in --test_filter as before.
    
    When a test name is detected, the plugin will instead set --test_arg=-t
    --test_arg="testName", and leave --test_filter out. This is because
    rules_scala turns --test_filter into the -s Scalatest runner parameter,
    which selects a full class for execution. Passing both -s and -t would
    cause Scalatest to run both the full class, as well as the selected test.
    
    Fixes #3425

--- scala/src/com/google/idea/blaze/scala/run/producers/ScalaTestContextProvider.java
+++ scala/src/com/google/idea/blaze/scala/run/producers/ScalaTestContextProvider.java
@@ -16,6 +16,7 @@
 package com.google.idea.blaze.scala.run.producers;
 
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.idea.blaze.base.command.BlazeFlags;
 import com.google.idea.blaze.base.dependencies.TargetInfo;
 import com.google.idea.blaze.base.run.ExecutorType;
 import com.google.idea.blaze.base.run.TestTargetHeuristic;
@@ -23,18 +24,18 @@ import com.google.idea.blaze.base.run.producers.RunConfigurationContext;
 import com.google.idea.blaze.base.run.producers.TestContext;
 import com.google.idea.blaze.base.run.producers.TestContextProvider;
 import com.google.idea.blaze.base.run.smrunner.SmRunnerUtils;
+import com.google.idea.blaze.base.run.state.RunConfigurationFlagsState;
 import com.google.idea.blaze.java.run.producers.TestSizeFinder;
-import com.intellij.codeInsight.TestFrameworks;
 import com.intellij.execution.JavaExecutionUtil;
 import com.intellij.execution.Location;
 import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.testIntegration.TestFramework;
-import javax.annotation.Nullable;
-import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass;
+import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScTypeDefinition;
+import org.jetbrains.plugins.scala.testingSupport.test.scalatest.ScalaTestConfigurationProducer;
 import org.jetbrains.plugins.scala.testingSupport.test.scalatest.ScalaTestTestFramework;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * {@link TestContextProvider} for run configurations related to Scala test classes (not handled by
@@ -45,25 +46,52 @@ class ScalaTestContextProvider implements TestContextProvider {
   @Nullable
   @Override
   public RunConfigurationContext getTestContext(ConfigurationContext context) {
-    ScClass testClass = getTestClass(context);
-    if (testClass == null) {
+    ClassAndTestName classAndTestName = getTestClass(context);
+
+    if (classAndTestName == null) {
       return null;
     }
+
+    ScTypeDefinition testClass = classAndTestName.testClass;
+
     ListenableFuture<TargetInfo> target =
         TestTargetHeuristic.targetFutureForPsiElement(
             testClass, TestSizeFinder.getTestSize(testClass));
     if (target == null) {
       return null;
     }
-    return TestContext.builder(testClass, ExecutorType.DEBUG_SUPPORTED_TYPES)
+
+    TestContext.Builder builder = TestContext.builder(testClass, ExecutorType.DEBUG_SUPPORTED_TYPES)
         .setTarget(target)
-        .setTestFilter(getTestFilter(testClass))
-        .setDescription(testClass.getName())
-        .build();
+        .setDescription(testClass.getName());
+
+    if (classAndTestName.testName != null) {
+      return builder
+          //This causes -t to be added to the Scalatest runner, selecting the relevant test case
+          //Setting -s as well would cause the entire test class to run, so leave the test filter unset.
+          .addBlazeFlagsModification(setScalatestSingleTestNameFlag(classAndTestName.testName))
+          .build();
+    } else {
+      return builder
+          //This causes -s to be added to the Scalatest runner, selecting the relevant class
+          .setTestFilter(getTestFilter(testClass))
+          .build();
+    }
+  }
+
+  private static class ClassAndTestName {
+    private final ScTypeDefinition testClass;
+    @Nullable
+    private final String testName;
+
+    public ClassAndTestName(ScTypeDefinition testClass, @Nullable String testName) {
+      this.testClass = testClass;
+      this.testName = testName;
+    }
   }
 
   @Nullable
-  private static ScClass getTestClass(ConfigurationContext context) {
+  private static ClassAndTestName getTestClass(ConfigurationContext context) {
     Location<?> location = context.getLocation();
     if (location == null) {
       return null;
@@ -76,27 +104,31 @@ class ScalaTestContextProvider implements TestContextProvider {
       // handled by a different producer
       return null;
     }
-    return getTestClass(location);
+    ScalaTestConfigurationProducer scalaTestConfigurationProducer = new ScalaTestConfigurationProducer();
+    return scalaTestConfigurationProducer.getTestClassWithTestName(location)
+        .map(classWithTestName -> new ClassAndTestName(classWithTestName.testClass(), classWithTestName.testName().getOrElse(() -> null)))
+        .getOrElse(() -> null);
   }
 
-  @Nullable
-  private static ScClass getTestClass(Location<?> location) {
-    PsiElement element = location.getPsiElement();
-    ScClass testClass;
-    if (element instanceof ScClass) {
-      testClass = (ScClass) element;
-    } else {
-      testClass = PsiTreeUtil.getParentOfType(element, ScClass.class);
-    }
-    if (testClass != null && isTestClass(testClass)) {
-      return testClass;
-    }
-    return null;
-  }
+  static TestContext.BlazeFlagsModification setScalatestSingleTestNameFlag(String testName) {
+    String testSelectorFlag = BlazeFlags.TEST_ARG + "-t";
+    //Scalatest names can contain spaces, so the name needs to be quoted
+    String testNameFlag = BlazeFlags.TEST_ARG + "\"" + testName + "\"";
+    return new TestContext.BlazeFlagsModification() {
+      @Override
+      public void modifyFlags(List<String> flags) {
+        if (!flags.contains(testSelectorFlag) && !flags.contains(testNameFlag)) {
+          flags.add(testSelectorFlag);
+          flags.add(testNameFlag);
+        }
+      }
 
-  private static boolean isTestClass(ScClass testClass) {
-    TestFramework framework = TestFrameworks.detectFramework(testClass);
-    return framework instanceof ScalaTestTestFramework && framework.isTestClass(testClass);
+      @Override
+      public boolean matchesConfigState(RunConfigurationFlagsState state) {
+        List<String> rawFlags = state.getRawFlags();
+        return rawFlags.contains(testSelectorFlag) && rawFlags.contains(testNameFlag);
+      }
+    };
   }
 
   private static String getTestFilter(PsiClass testClass) {
