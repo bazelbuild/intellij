fc5e12c62f6552238a52c8b062a226425eb8911d
Alexey Györi <agy@google.com>
2022-08-05 07:33:13 -0700

    Introduce a setting to always go to the newest child task in Blaze view
    (cherry picked from commit 266efef1a31ee9ce06fd378f02573509cb98587a)

--- base/src/com/google/idea/blaze/base/settings/ui/BlazeUserSettingsConfigurable.java
+++ base/src/com/google/idea/blaze/base/settings/ui/BlazeUserSettingsConfigurable.java
@@ -102,6 +102,12 @@ public class BlazeUserSettingsConfigurable extends AutoConfigurable {
           .setter(BlazeUserSettings::setCollapseProjectView)
           .componentFactory(SimpleComponent::createCheckBox);
 
+  private static final ConfigurableSetting<?, ?> ALWAYS_SELECT_NEWEST_CHILD_TASK =
+      setting("Always select the newest child task in Blaze view")
+          .getter(BlazeUserSettings::getSelectNewestChildTask)
+          .setter(BlazeUserSettings::setSelectNewestChildTask)
+          .componentFactory(SimpleComponent::createCheckBox);
+
   private static final ConfigurableSetting<?, ?> FORMAT_BUILD_FILES_ON_SAVE =
       setting("Automatically format BUILD/Starlark files on file save")
           .getter(BlazeUserSettings::getFormatBuildFilesOnSave)
@@ -147,6 +153,7 @@ public class BlazeUserSettingsConfigurable extends AutoConfigurable {
           COLLAPSE_PROJECT_VIEW,
           FORMAT_BUILD_FILES_ON_SAVE,
           SHOW_ADD_FILE_TO_PROJECT,
+          ALWAYS_SELECT_NEWEST_CHILD_TASK,
           BLAZE_BINARY_PATH,
           BAZEL_BINARY_PATH,
           BUILDIFIER_BINARY_PATH);
@@ -175,6 +182,7 @@ public class BlazeUserSettingsConfigurable extends AutoConfigurable {
             COLLAPSE_PROJECT_VIEW,
             FORMAT_BUILD_FILES_ON_SAVE,
             SHOW_ADD_FILE_TO_PROJECT,
+            ALWAYS_SELECT_NEWEST_CHILD_TASK,
             BLAZE_BINARY_PATH,
             BAZEL_BINARY_PATH,
             BUILDIFIER_BINARY_PATH));
