516365223d921f8927961f794043950ca3756c35
Googler <intellij-github@google.com>
2022-08-09 17:02:39 -0700

    mobile-install: let the build step decide whether the IDE should deploy
    (cherry picked from commit 3471b97b3b1e6b98dcfbf9546dcb3ad0fa362c76)

--- aswb/src/com/google/idea/blaze/android/run/binary/mobileinstall/MobileInstallBuildStep.java
+++ aswb/src/com/google/idea/blaze/android/run/binary/mobileinstall/MobileInstallBuildStep.java
@@ -263,6 +263,11 @@ public class MobileInstallBuildStep implements ApkBuildStep {
   }
 
   @Override
+  public boolean needsIdeDeploy() {
+    return StudioDeployerExperiment.isEnabled();
+  }
+
+  @Override
   public BlazeAndroidDeployInfo getDeployInfo() throws ApkProvisionException {
     if (deployInfo != null) {
       return deployInfo;
