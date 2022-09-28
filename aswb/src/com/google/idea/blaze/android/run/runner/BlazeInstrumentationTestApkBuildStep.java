05767bf4391bd06c23e614ab622d8e3913fd50c6
Googler <intellij-github@google.com>
2022-09-28 09:03:16 -0700

    Make InstrumentationInfo a top level class
    This change just moves the InstrumentationInfo class and the
    code to generate it from a `android_instrumentation_test` target
    to be a top level class.
    
    PiperOrigin-RevId: 477466892

--- aswb/src/com/google/idea/blaze/android/run/runner/BlazeInstrumentationTestApkBuildStep.java
+++ aswb/src/com/google/idea/blaze/android/run/runner/BlazeInstrumentationTestApkBuildStep.java
@@ -32,11 +32,6 @@ import com.google.idea.blaze.base.command.buildresult.BuildResultHelper;
 import com.google.idea.blaze.base.command.buildresult.BuildResultHelper.GetArtifactsException;
 import com.google.idea.blaze.base.console.BlazeConsoleLineProcessorProvider;
 import com.google.idea.blaze.base.filecache.FileCaches;
-import com.google.idea.blaze.base.ideinfo.AndroidIdeInfo;
-import com.google.idea.blaze.base.ideinfo.AndroidInstrumentationInfo;
-import com.google.idea.blaze.base.ideinfo.TargetIdeInfo;
-import com.google.idea.blaze.base.ideinfo.TargetKey;
-import com.google.idea.blaze.base.ideinfo.TargetMap;
 import com.google.idea.blaze.base.model.BlazeProjectData;
 import com.google.idea.blaze.base.model.primitives.Label;
 import com.google.idea.blaze.base.model.primitives.WorkspaceRoot;
@@ -48,10 +43,8 @@ import com.google.idea.blaze.base.sync.aspects.BlazeBuildOutputs;
 import com.google.idea.blaze.base.sync.aspects.BuildResult;
 import com.google.idea.blaze.base.sync.data.BlazeProjectDataManager;
 import com.google.idea.blaze.base.util.SaveUtil;
-import com.google.idea.blaze.java.AndroidBlazeRules.RuleTypes;
 import com.intellij.openapi.project.Project;
 import java.io.File;
-import javax.annotation.Nullable;
 
 /** Builds the APKs required for an android instrumentation test. */
 public class BlazeInstrumentationTestApkBuildStep implements ApkBuildStep {
@@ -95,7 +88,7 @@ public class BlazeInstrumentationTestApkBuildStep implements ApkBuildStep {
     }
 
     InstrumentationInfo testComponents =
-        getInstrumentationInfo(instrumentationTestLabel, projectData, context);
+        InstrumentationInfo.getInstrumentationInfo(instrumentationTestLabel, projectData, context);
     if (testComponents == null) {
       return;
     }
@@ -185,111 +178,5 @@ public class BlazeInstrumentationTestApkBuildStep implements ApkBuildStep {
     throw new ApkProvisionException(
         "Failed to read APK deploy info.  Either build step hasn't been executed or there was an"
             + " error obtaining deploy info after build.");
-  }
-
-  /**
-   * Extracts information about the test and target apps from the instrumentation test rule.
-   *
-   * @return The labels contained in an {@link InstrumentationInfo} object.
-   */
-  @Nullable
-  @VisibleForTesting
-  public static InstrumentationInfo getInstrumentationInfo(
-      Label instrumentationTestLabel, BlazeProjectData projectData, BlazeContext context) {
-    // The following extracts the dependency info required during an instrumentation test.
-    // To disambiguate, we try to follow the same terminology as used by the
-    // android_instrumentation_test rule docs:
-    // - test: The android_instrumentation_test target.
-    // - test_app: The target of kind android_binary that's used as the binary that
-    // orchestrates the instrumentation test.
-    // - target_app: The android_binary app that's being tested by the test_app.
-    TargetMap targetMap = projectData.getTargetMap();
-    TargetIdeInfo testTarget = targetMap.get(TargetKey.forPlainTarget(instrumentationTestLabel));
-    if (testTarget == null
-        || testTarget.getKind() != RuleTypes.ANDROID_INSTRUMENTATION_TEST.getKind()) {
-      IssueOutput.error(
-              "Unable to identify target \""
-                  + instrumentationTestLabel
-                  + "\". Please sync the project and try again.")
-          .submit(context);
-      return null;
-    }
-    AndroidInstrumentationInfo testInstrumentationInfo = testTarget.getAndroidInstrumentationInfo();
-    if (testInstrumentationInfo == null) {
-      IssueOutput.error(
-              "Required target data missing for \""
-                  + instrumentationTestLabel
-                  + "\".  Has the target definition changed recently? Please sync the project and"
-                  + " try again.")
-          .submit(context);
-      return null;
-    }
-
-    Label testApp = testInstrumentationInfo.getTestApp();
-    if (testApp == null) {
-      IssueOutput.error(
-              "No \"test_app\" in target definition for "
-                  + testTarget.getKey().getLabel()
-                  + ". Please ensure \"test_app\" attribute is set.  See"
-                  + " https://docs.bazel.build/versions/master/be/android.html#android_instrumentation_test.test_app"
-                  + " for more information.")
-          .submit(context);
-      return null;
-    }
-
-    TargetIdeInfo testAppTargetIdeInfo = targetMap.get(TargetKey.forPlainTarget(testApp));
-    if (testAppTargetIdeInfo == null) {
-      IssueOutput.error(
-              "Unable to identify target \""
-                  + testApp
-                  + "\". Please sync the project and try again.")
-          .submit(context);
-      return null;
-    }
-    AndroidIdeInfo testAppAndroidInfo = testAppTargetIdeInfo.getAndroidIdeInfo();
-    if (testAppAndroidInfo == null) {
-      IssueOutput.error(
-              "Required target data missing for \""
-                  + testApp
-                  + "\".  Has the target definition changed recently? Please sync the project and"
-                  + " try again.")
-          .submit(context);
-      return null;
-    }
-    Label targetApp = testAppAndroidInfo.getInstruments();
-    return new InstrumentationInfo(targetApp, testApp);
-  }
-
-  /**
-   * Container for information about {@code android_instrumentation_test}: it holds links to the
-   * test target and the instrumented target.
-   */
-  @VisibleForTesting
-  public static class InstrumentationInfo {
-    /**
-     * {@code android_binary} target corresponding to the application under test.
-     *
-     * <p>This is obtained from the {@code instruments} attribute of the binary that contains the
-     * tests.
-     */
-    @Nullable public final Label targetApp;
-
-    /**
-     * {@code android_binary} target that contains the instrumentation tests.
-     *
-     * <p>This is obtained from the {@code test_app} attribute of an {@code
-     * android_instrumentation_test} target.
-     */
-    public final Label testApp;
-
-    InstrumentationInfo(@Nullable Label targetApp, Label testApp) {
-      this.targetApp = targetApp;
-      this.testApp = testApp;
-    }
-
-    /** Returns whether the test app contains the target itself (self-instrumenting). */
-    public boolean isSelfInstrumentingTest() {
-      return targetApp == null;
-    }
   }
 }
