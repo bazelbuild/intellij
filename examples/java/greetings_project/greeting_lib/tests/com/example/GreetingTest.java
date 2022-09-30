1ffc7c6f74c66ad353e1bfd61c592e2ec89488b9
Mai Hussien <messa@google.com>
2022-09-29 19:12:32 -0700

    Apply review comments

--- examples/java/greetings_project/greeting_lib/tests/com/example/GreetingTest.java
+++ examples/java/greetings_project/greeting_lib/tests/com/example/GreetingTest.java
@@ -6,24 +6,29 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class GreetingTest {
    @Test
     public void testGreetNoNames() {
-        List<String> got = Greeting.getGreetings("greetings", Collections.emptyList());
-       assertThat(got).isEmpty();
-    }
+       List<String> got = Greeting.getGreetings("greetings", Collections.emptyList());
+       assertEquals(0, got.size());
+   }
 
     @Test
     public void testGreetOneName() {
         List<String> got = Greeting.getGreetings("greetings", Collections.singletonList("name"));
-        assertThat(got).containsExactly("greetings name!");
-    }
+        assertEquals(1, got.size());
+        assertEquals("greetings name!", got.get(0));
+   }
 
     @Test
     public void testGreetMultipleNames() {
-        List<String> got = Greeting.getGreetings("greetings", Arrays.asList("name1", "name2"));
-        assertThat(got).containsExactly("greetings name1!", "greetings name2!");
+        List<String> got =
+                Greeting.getGreetings("greetings", Arrays.asList("name1", "name2", "name3"));
+        assertEquals(3, got.size());
+        assertEquals("greetings name1!", got.get(0));
+        assertEquals("greetings name2!", got.get(1));
+        assertEquals("greetings name3!", got.get(2));
     }
 }
