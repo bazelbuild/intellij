b7de8af67a457680088aefc8d60287631c9bb97b
Mai Hussien <70515749+mai93@users.noreply.github.com>
2022-11-28 12:17:43 -0500

    Mock bazel run in Gazelle integration tests (#4129)
    * Use mock GazelleRunner for tests
    
    Co-authored-by: Borja Lorente <blorente@apple.com>

--- gazelle/src/com/google/idea/blaze/gazelle/GazelleRunnerImpl.java
+++ gazelle/src/com/google/idea/blaze/gazelle/GazelleRunnerImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2022 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.idea.blaze.gazelle;
 
 import com.google.common.collect.ImmutableList;
@@ -17,7 +32,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class BlazeGazelleRunnerImpl extends BlazeGazelleRunner {
+public class GazelleRunnerImpl extends GazelleRunner {
 
   @Override
   public GazelleRunResult runBlazeGazelle(
@@ -28,13 +43,8 @@ public class BlazeGazelleRunnerImpl extends BlazeGazelleRunner {
       Label gazelleTarget,
       Collection<WorkspacePath> directories,
       ImmutableList<Parser> issueParsers) {
-    BlazeCommand.Builder builder = BlazeCommand.builder(invoker, BlazeCommandName.RUN);
-    builder.addBlazeFlags(blazeFlags);
-    builder.addTargets(gazelleTarget);
-    List<String> directoriesToRegenerate =
-        directories.stream().map(WorkspacePath::toString).collect(Collectors.toList());
-    builder.addExeFlags(directoriesToRegenerate);
-    BlazeCommand command = builder.build();
+    BlazeCommand command = GazelleRunner.createGazelleRunCommand(invoker,
+            blazeFlags, gazelleTarget, directories);
     ByteArrayOutputStream stdout = new ByteArrayOutputStream();
     int exitCode =
         ExternalTask.builder(workspaceRoot)
