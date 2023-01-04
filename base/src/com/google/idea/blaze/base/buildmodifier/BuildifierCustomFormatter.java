f08795f8eb18da91d88be13f91b29cb0681dce8c
Yasmine Mohamed <ymoh@google.com>
2023-01-04 08:10:24 -0800

    Use new Formatter API for buildifier formatter.
    (cherry picked from commit fdeed6890b7a792e291f14b7480c4ed1ac3a31e7)

--- base/src/com/google/idea/blaze/base/buildmodifier/BuildifierCustomFormatter.java
+++ base/src/com/google/idea/blaze/base/buildmodifier/BuildifierCustomFormatter.java
@@ -15,6 +15,8 @@
  */
 package com.google.idea.blaze.base.buildmodifier;
 
+import static com.google.idea.blaze.base.buildmodifier.BuildifierFormattingService.useNewBuildifierFormattingService;
+
 import com.google.idea.blaze.base.formatter.CustomFormatter;
 import com.google.idea.blaze.base.formatter.FormatUtils.FileContentsProvider;
 import com.google.idea.blaze.base.formatter.FormatUtils.Replacements;
@@ -31,7 +33,7 @@ final class BuildifierCustomFormatter implements CustomFormatter {
 
   @Override
   public boolean appliesToFile(Project project, PsiFile file) {
-    return file instanceof BuildFile;
+    return !useNewBuildifierFormattingService.isEnabled() && file instanceof BuildFile;
   }
 
   @Nullable
