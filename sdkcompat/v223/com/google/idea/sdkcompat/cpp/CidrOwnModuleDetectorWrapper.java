033e336b300e22764d627fff3020bcd19ae18c79
Tomasz Pasternak <1066003+tpasternak@users.noreply.github.com>
2023-01-04 12:36:25 -0800

    Mark module as clion's own module (#4250)
    This prevents "This file does not belong to any project target;
    code insight features might not work properly" popup
    
    closes #4168

---
+++ sdkcompat/v223/com/google/idea/sdkcompat/cpp/CidrOwnModuleDetectorWrapper.java
@@ -0,0 +1,9 @@
+package com.google.idea.sdkcompat.cpp;
+
+import com.jetbrains.cidr.project.workspace.CidrOwnModuleDetector;
+
+/**
+ * #api222
+ */
+public interface CidrOwnModuleDetectorWrapper extends CidrOwnModuleDetector {
+}
