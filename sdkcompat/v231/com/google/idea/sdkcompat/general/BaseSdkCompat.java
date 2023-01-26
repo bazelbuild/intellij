f109930eed9ec12d1a44b6cc8a3598628b9fbf44
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2023-01-26 18:06:54 +0100

    Drop api212 in sdkcompat v231
    So  skdcompat/v231 is now aligned with changes introduced in 00b4999d7cceae7073cf0053f047a954c765c6fa

--- sdkcompat/v231/com/google/idea/sdkcompat/general/BaseSdkCompat.java
+++ sdkcompat/v231/com/google/idea/sdkcompat/general/BaseSdkCompat.java
@@ -14,8 +14,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.DependencyScope;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.libraries.Library;
-import com.intellij.openapi.ui.TextComponentAccessor;
-import com.intellij.openapi.ui.TextComponentAccessors;
 import com.intellij.openapi.vcs.FilePath;
 import com.intellij.openapi.vcs.changes.ui.ChangesListView;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -23,7 +21,6 @@ import com.intellij.refactoring.rename.RenamePsiElementProcessor;
 import com.intellij.refactoring.rename.RenamePsiElementProcessorBase;
 import com.intellij.ui.CoreIconManager;
 import com.intellij.ui.IconManager;
-import com.intellij.ui.TextFieldWithStoredHistory;
 import com.intellij.usageView.UsageTreeColors;
 import com.intellij.usages.TextChunk;
 import com.intellij.util.Restarter;
@@ -42,11 +39,6 @@ import javax.annotation.Nullable;
 /** Provides SDK compatibility shims for base plugin API classes, available to all IDEs. */
 public final class BaseSdkCompat {
   private BaseSdkCompat() {}
-
-  /** #api212: inline into FileSelectorWithStoredHistory */
-  public static final TextComponentAccessor<TextFieldWithStoredHistory>
-      TEXT_FIELD_WITH_STORED_HISTORY_WHOLE_TEXT =
-          TextComponentAccessors.TEXT_FIELD_WITH_STORED_HISTORY_WHOLE_TEXT;
 
   /** #api203: refactor this function back into CodesearchResultData and make it private. */
   public static void addLineNumber(int lineNumber, List<TextChunk> chunks) {
