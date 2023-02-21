3566ae23752ac0242f93a8c23373df4a0e83df1e
Googler <intellij-github@google.com>
2023-02-21 15:08:42 -0800

    Branch asdev compat code into as223
    PiperOrigin-RevId: 511316705

---
+++ aswb/sdkcompat/as223/com/google/idea/blaze/android/run/test/BlazeAndroidTestProgramRunner.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.idea.blaze.android.run.test;
+
+import com.google.idea.blaze.android.run.BlazeAndroidRunConfigurationHandler;
+import com.google.idea.blaze.base.run.BlazeCommandRunConfiguration;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.ExecutionResult;
+import com.intellij.execution.configurations.RunProfile;
+import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.configurations.RunnerSettings;
+import com.intellij.execution.executors.DefaultDebugExecutor;
+import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.execution.runners.ExecutionEnvironment;
+import com.intellij.execution.runners.GenericProgramRunner;
+import com.intellij.execution.runners.RunContentBuilder;
+import com.intellij.execution.ui.RunContentDescriptor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+
+/** Program runner for configurations from {@link BlazeAndroidTestRunConfigurationHandler}. */
+public class BlazeAndroidTestProgramRunner extends GenericProgramRunner<RunnerSettings> {
+  @Override
+  public boolean canRun(String executorId, RunProfile profile) {
+    BlazeAndroidRunConfigurationHandler handler =
+        BlazeAndroidRunConfigurationHandler.getHandlerFrom(profile);
+    if (!(handler instanceof BlazeAndroidTestRunConfigurationHandler)) {
+      return false;
+    }
+    if (!(profile instanceof BlazeCommandRunConfiguration)) {
+      return false;
+    }
+    return DefaultRunExecutor.EXECUTOR_ID.equals(executorId)
+        || DefaultDebugExecutor.EXECUTOR_ID.equals(executorId);
+  }
+
+  @Override
+  protected RunContentDescriptor doExecute(
+      final RunProfileState state, final ExecutionEnvironment env) throws ExecutionException {
+    FileDocumentManager.getInstance().saveAllDocuments();
+    ExecutionResult result = state.execute(env.getExecutor(), this);
+    return new RunContentBuilder(result, env).showRunContent(env.getContentToReuse());
+  }
+
+  @Override
+  public String getRunnerId() {
+    return "AndroidTestProgramRunner";
+  }
+}
