12c20e105cc66d1279eb0df28d7d921d5d3d4c68
Tomasz <tomasz.pasternak@jetbrains.com>
2023-05-08 17:13:29 +0200

    plugin_dev: Allow plugin development on Windows

--- plugin_dev/src/com/google/idea/blaze/plugin/run/IntellijWithPluginClasspathHelper.java
+++ plugin_dev/src/com/google/idea/blaze/plugin/run/IntellijWithPluginClasspathHelper.java
@@ -184,7 +184,11 @@ public class IntellijWithPluginClasspathHelper {
     String appPackage = Path.of(ideaJdk.getHomePath()).getParent().toString();
     if (launch != null && launch.additionalJvmArguments != null) {
       for (String arg : launch.additionalJvmArguments) {
-        arg = arg.replace("$IDE_HOME", ideaJdk.getHomePath());
+        if(SystemInfo.isWindows) {
+          arg = arg.replace("%IDE_HOME%", ideaJdk.getHomePath());
+        } else {
+          arg = arg.replace("$IDE_HOME", ideaJdk.getHomePath());
+        }
         if (SystemInfo.isMac) {
           // Perform replacements that are done by the mac launcher here
           arg = arg.replace("$APP_PACKAGE", appPackage);
