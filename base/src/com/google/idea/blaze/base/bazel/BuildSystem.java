fe2e8a63a9a80f9292dad69b063933d405a10c2b
Googler <intellij-github@google.com>
2023-05-09 22:57:54 -0700

    Update blaze invocation workflows for debugging local tests
    (cherry picked from commit c596b4fc8a100466eba7fcd800ae2a53a4fccb7f)

--- base/src/com/google/idea/blaze/base/bazel/BuildSystem.java
+++ base/src/com/google/idea/blaze/base/bazel/BuildSystem.java
@@ -20,7 +20,9 @@ import com.google.idea.blaze.base.command.BlazeCommandRunner;
 import com.google.idea.blaze.base.command.buildresult.BuildResultHelper;
 import com.google.idea.blaze.base.command.info.BlazeInfo;
 import com.google.idea.blaze.base.model.BlazeVersionData;
+import com.google.idea.blaze.base.model.primitives.Kind;
 import com.google.idea.blaze.base.model.primitives.WorkspaceRoot;
+import com.google.idea.blaze.base.run.ExecutorType;
 import com.google.idea.blaze.base.scope.BlazeContext;
 import com.google.idea.blaze.base.settings.BuildBinaryType;
 import com.google.idea.blaze.base.settings.BuildSystemName;
@@ -89,6 +91,15 @@ public interface BuildSystem {
 
   /** Get a Blaze invoker. */
   BuildInvoker getBuildInvoker(Project project, BlazeContext context);
+  /** Get a Blaze invoker specific to executor type and run config. */
+  default BuildInvoker getBuildInvoker(
+      Project project, BlazeContext context, ExecutorType executorType, Kind targetKind) {
+    throw new UnsupportedOperationException(
+        String.format(
+            "The getBuildInvoker method specific to executor type and target kind is not"
+                + " implemented in %s",
+            this.getClass().getSimpleName()));
+  }
 
   /** Get a Blaze invoker that only run build locally. */
   Optional<BuildInvoker> getLocalBuildInvoker(Project project, BlazeContext context);
