71e48a7f5b2b7dd3e9896f3bdf509bba3ef920f4
Googler <intellij-github@google.com>
2023-07-10 12:12:31 -0700

    Add "GotIt" popup to the build dependencies action
    As discussed with the UX team, this change adds
    a pop up to educate users on what build dependencies
    does. The pop up shows only once, and when dismissed
    it never pops up again.
    
    The pop up is the same used for reader mode and other
    notifications in the IDE. This has the advantage
    that when the reader mode pop up shows up, the
    system will wait to not show both at the same time.
    
    (cherry picked from commit e137195564594a6d34658bc9628f3f9b50b264fa)

--- base/src/com/google/idea/blaze/base/qsync/QuerySyncInspectionWidgetActionProvider.java
+++ base/src/com/google/idea/blaze/base/qsync/QuerySyncInspectionWidgetActionProvider.java
@@ -30,15 +30,23 @@ import com.intellij.openapi.editor.EditorKind;
 import com.intellij.openapi.editor.markup.InspectionWidgetActionProvider;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
+import com.intellij.ui.GotItTooltip;
 import com.intellij.ui.scale.JBUIScale;
 import com.intellij.util.ui.JBUI;
+import java.awt.Point;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.HierarchyBoundsListener;
+import java.awt.event.HierarchyEvent;
 import java.util.Set;
 import javax.swing.JComponent;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.plaf.FontUIResource;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -135,7 +143,100 @@ public class QuerySyncInspectionWidgetActionProvider implements InspectionWidget
                       button.getFont().getStyle(),
                       button.getFont().getSize() - JBUIScale.scale(2))));
 
+      createGotItTooltip(button);
+
       return button;
+    }
+
+    private void createGotItTooltip(ActionButtonWithText button) {
+      Project project = editor.getProject();
+      if (project != null) {
+        GotItTooltip gotIt =
+            new GotItTooltip(
+                    "query.sync.got.it",
+                    "ASwB no longer builds your entire project during sync. Now you decide whether"
+                        + " to build file dependencies for advanced code editing features or not."
+                        + " Without building dependencies, you can still navigate your codebase and"
+                        + " make light code edits. For analysis and deeper code editing, click the"
+                        + " hammer icon to build this file's dependencies. To learn more about"
+                        + " these changes: go/query-sync",
+                    project)
+                .withHeader("Welcome to query sync");
+        // Ideally we would attach the balloon to the button, however the balloon disappears when
+        // the component it's attached to is hidden. Unfortunatelly the visibility of traffic
+        // light components is flaky and they pop in and out of existence causing the pop up to
+        // go away almost immediately. To solve this the reader mode one, attempts to wait until
+        // the traffic light settles (see:
+        // https://github.com/JetBrains/intellij-community/blob/155fabab65515352ea782dca0bc22ac1e67bde43/platform/lang-impl/src/com/intellij/codeInsight/actions/ReaderModeActionProvider.kt#L115
+        //
+        // Here we use a different approach, instead of attaching to the button we attach to the
+        // editor and provide a position provider to track the button instead. This results
+        // in a more stable behaviour without attaching to the global message bus.
+        gotIt
+            .setOnBalloonCreated(
+                balloon -> {
+                  // Add the listeners to track the button's position.
+                  button.addHierarchyBoundsListener(new BalloonHierarchyBoundsListener(balloon));
+                  button.addComponentListener(new BalloonComponentListener(balloon));
+                  return null;
+                })
+            .show(
+                editor.getContentComponent(),
+                (component, balloon) -> {
+                  Point p = SwingUtilities.convertPoint(button, 0, 0, component);
+                  Point pos2 = new Point(p.x + button.getWidth() / 2, p.y + button.getHeight());
+
+                  pos2.x = Math.min(Math.max(pos2.x, 0), component.getWidth());
+                  pos2.y = Math.min(Math.max(pos2.y, 0), component.getHeight());
+                  return pos2;
+                });
+      }
+    }
+
+    private static class BalloonComponentListener implements ComponentListener {
+
+      private final Balloon balloon;
+
+      public BalloonComponentListener(Balloon balloon) {
+        this.balloon = balloon;
+      }
+
+      @Override
+      public void componentResized(ComponentEvent componentEvent) {
+        balloon.revalidate();
+      }
+
+      @Override
+      public void componentMoved(ComponentEvent componentEvent) {
+        balloon.revalidate();
+      }
+
+      @Override
+      public void componentShown(ComponentEvent componentEvent) {
+        balloon.revalidate();
+      }
+
+      @Override
+      public void componentHidden(ComponentEvent componentEvent) {}
+    }
+
+    private static class BalloonHierarchyBoundsListener implements HierarchyBoundsListener {
+
+      private final Balloon balloon;
+
+      public BalloonHierarchyBoundsListener(Balloon balloon) {
+        this.balloon = balloon;
+      }
+
+      @Override
+      public void ancestorMoved(HierarchyEvent hierarchyEvent) {
+        balloon.revalidate();
+      }
+
+      @Override
+      public void ancestorResized(HierarchyEvent hierarchyEvent) {
+        balloon.revalidate();
+      }
     }
   }
 }
