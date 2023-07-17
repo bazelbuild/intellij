5fbd5cf4539c315d2d7581b1baa7d9a646f5fedf
dkashyn <dkashyn@salesforce.com>
2023-07-17 18:07:47 -0400

    Select source roots for library jars that have interim folders like `/src/...`. IJ UI logic is reused and suggested candidate selected if and only if there is one candidate exists.

--- base/src/com/google/idea/blaze/base/sync/libraries/LibraryModifier.java
+++ base/src/com/google/idea/blaze/base/sync/libraries/LibraryModifier.java
@@ -20,15 +20,20 @@ import com.google.idea.blaze.base.model.BlazeProjectData;
 import com.google.idea.blaze.base.model.LibraryFilesProvider;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
+import com.intellij.openapi.progress.EmptyProgressIndicator;
 import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.Library.ModifiableModel;
+import com.intellij.openapi.roots.ui.configuration.JavaVfsSourceRootDetectionUtil;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.io.FileUtilRt;
 import com.intellij.openapi.vfs.StandardFileSystems;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.util.io.URLUtil;
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 /** Modifies {@link Library} content in {@link Library.ModifiableModel}. */
 public class LibraryModifier {
@@ -73,7 +78,20 @@ public class LibraryModifier {
       logger.warn("No local file found for " + file);
       return;
     }
-    modifiableModel.addRoot(pathToUrl(file), orderRootType);
+    if (orderRootType == OrderRootType.SOURCES) {
+      VirtualFile jarfile = VirtualFileManager.getInstance().findFileByUrl(pathToUrl(file));
+      List<VirtualFile> candidates = Collections.emptyList();
+      if (jarfile != null) {
+        candidates = JavaVfsSourceRootDetectionUtil.suggestRoots(jarfile, new EmptyProgressIndicator());
+      }
+      if (candidates.size() == 1) {
+        modifiableModel.addRoot(candidates.get(0), orderRootType);
+      } else {
+        modifiableModel.addRoot(pathToUrl(file), orderRootType);
+      }
+    } else {
+      modifiableModel.addRoot(pathToUrl(file), orderRootType);
+    }
   }
 
   private String pathToUrl(File path) {
