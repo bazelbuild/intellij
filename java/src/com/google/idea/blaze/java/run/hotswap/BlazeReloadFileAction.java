e6829d94bf9096e7063bb26d9dc7e352f8f0e48d
Brian McNamara <brian.mcnamara@salesforce.com>
2023-07-25 19:33:24 +0000

    Cleanup and refactor code

--- java/src/com/google/idea/blaze/java/run/hotswap/BlazeReloadFileAction.java
+++ java/src/com/google/idea/blaze/java/run/hotswap/BlazeReloadFileAction.java
@@ -2,10 +2,10 @@ package com.google.idea.blaze.java.run.hotswap;
 
 import com.google.common.collect.ImmutableList;
 import com.google.idea.blaze.base.build.BlazeBuildService;
-import com.google.idea.blaze.base.command.buildresult.BepArtifactData;
 import com.google.idea.blaze.base.command.buildresult.LocalFileOutputArtifact;
 import com.google.idea.blaze.base.model.primitives.Label;
 import com.google.idea.blaze.base.sync.aspects.BlazeBuildOutputs;
+import com.google.idea.blaze.base.sync.aspects.BuildResult;
 import com.google.idea.blaze.base.targetmaps.SourceToTargetMap;
 import com.intellij.debugger.DebuggerManagerEx;
 import com.intellij.debugger.impl.DebuggerSession;
@@ -18,29 +18,30 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.ui.MessageCategory;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
-import java.nio.file.*;
-import java.util.Enumeration;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import java.util.jar.JarInputStream;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 
 public class BlazeReloadFileAction extends AnAction {
+
+    private static final Logger LOGGER = Logger.getInstance(BlazeReloadFileAction.class);
 
     public BlazeReloadFileAction(AnAction delegate) {
         super(
@@ -58,14 +59,13 @@ public class BlazeReloadFileAction extends AnAction {
         Future<BlazeBuildOutputs> blazeBuildOutputsFuture =
                 BlazeBuildService.getInstance(project).buildFile(vf.getName(), targets);
 
-
-        Path relative = ReadAction.compute(() ->
-                ProjectFileIndex.getInstance(project).getSourceRootForFile(vf).toNioPath().relativize(vf.toNioPath()));
         ApplicationManager.getApplication().executeOnPooledThread(() -> {
             try {
                 BlazeBuildOutputs outputs = blazeBuildOutputsFuture.get();
-                File temp = Files.createTempDirectory("IjBazelHotswap").toFile();
-                temp.deleteOnExit();
+                if (outputs.buildResult.status != BuildResult.Status.SUCCESS) {
+                    LOGGER.debug("Build failed during hotswap action, hotswap will not be performed");
+                    return;
+                }
 
                 ApplicationManager.getApplication().invokeLater(() -> {
                     HotSwapProgressImpl hotSwapProgress = new HotSwapProgressImpl(project);
@@ -74,61 +74,94 @@ public class BlazeReloadFileAction extends AnAction {
                     hotSwapProgress.setDebuggerSession(session);
 
                     ApplicationManager.getApplication().executeOnPooledThread(() -> {
-                        ProgressManager.getInstance().runProcess(() -> {
-                            try {
-                                String directory = relative.getParent().toString() + "/";
-                                String filenameWithoutExtension = vf.getName().substring(0, vf.getName().lastIndexOf('.'));
-
-                                outputs.artifacts.values().parallelStream()
-                                        .filter(value -> value.artifact instanceof LocalFileOutputArtifact && value.artifact.getRelativePath().endsWith(".jar"))
-                                        .map(value -> (LocalFileOutputArtifact) value.artifact)
-                                        .forEach(artifact -> {
-                                            ProgressManager.checkCanceled();
-                                            try (JarInputStream jis = new JarInputStream(new FileInputStream(artifact.getFile()))) {
-                                                ZipEntry entry;
-                                                while ((entry = jis.getNextEntry()) != null) {
-                                                    if (entry.isDirectory() && entry.getName().equals(directory)) {
-                                                        while ((entry = jis.getNextEntry()) != null && !entry.isDirectory()) {
-                                                            String entryFileName = entry.getName().substring(entry.getName().lastIndexOf(Paths.DELIM) + 1);
-                                                            String entryFileNameWithoutExtention = entryFileName.contains(".") ? entryFileName.substring(0, entryFileName.lastIndexOf('.')) : entryFileName;
-                                                            if (entryFileNameWithoutExtention.startsWith(filenameWithoutExtension) &&
-                                                                    (entryFileNameWithoutExtention.length() == filenameWithoutExtension.length() || entryFileNameWithoutExtention.charAt(filenameWithoutExtension.length()) == '$')) {
-                                                                File out = new File(temp, entryFileName);
-                                                                Files.copy(jis, out.toPath(), StandardCopyOption.REPLACE_EXISTING);
-
-                                                            }
-                                                        }
-                                                        break;
-                                                    }
-                                                }
-                                            } catch (IOException e) {
-                                                throw new RuntimeException(e);
-                                            }
-                                        });
-
-
-                                Map<DebuggerSession, Map<String, HotSwapFile>> sessionMap = new HashMap<>();
-                                Map<String, HotSwapFile> hotSwapFileMap = new HashMap<>();
-                                sessionMap.put(session, hotSwapFileMap);
-                                for (File file : temp.listFiles()) {
-                                    hotSwapFileMap.put(directory.replace(Paths.DELIM, '.') + file.getName().substring(0, file.getName().indexOf(".class")), new HotSwapFile(file));
-                                }
-
-                                ProgressManager.checkCanceled();
-                                if (session.isRunning()) {
-                                    HotSwapManager.reloadModifiedClasses(sessionMap, hotSwapProgress);
-                                }
-                            } finally {
-                                hotSwapProgress.finished();
-
-                            }
-                        }, hotSwapProgress.getProgressIndicator());
+                        findAndHotswapFile(project, vf, outputs, session, hotSwapProgress);
                     });
                 });
-            } catch (InterruptedException | ExecutionException | IOException e) {
-                throw new RuntimeException(e);
+            } catch (InterruptedException | ExecutionException e) {
+                LOGGER.error("Exception occurred during building file to hotswap", e);
             }
         });
+    }
 
+    private void findAndHotswapFile(Project project, VirtualFile sourceFile, BlazeBuildOutputs outputs,
+                                    DebuggerSession session, HotSwapProgressImpl progress) {
+        ProgressManager.getInstance().runProcess(() -> {
+            Path relative = ReadAction.compute(() ->
+                    ProjectFileIndex.getInstance(project)
+                            .getSourceRootForFile(sourceFile).toNioPath().
+                            relativize(sourceFile.toNioPath()));
+            File tempClassDir;
+            try {
+                tempClassDir = Files.createTempDirectory("IjBazelHotswap").toFile();
+                tempClassDir.deleteOnExit();
+            } catch (IOException e) {
+                LOGGER.error("Failed creating temp directory for hotswap", e);
+                progress.addMessage(session, MessageCategory.ERROR, "Failed setting environment up for hotswap");
+                progress.finished();
+                return;
+            }
+
+            try {
+                String directory = relative.getParent().toString() + "/";
+                String filenameWithoutExtension = sourceFile.getName().substring(0, sourceFile.getName().lastIndexOf('.'));
+
+                outputs.artifacts.values().parallelStream()
+                        .filter(value -> value.artifact instanceof LocalFileOutputArtifact && value.artifact.getRelativePath().endsWith(".jar"))
+                        .map(value -> (LocalFileOutputArtifact) value.artifact)
+                        .forEach(artifact -> {
+                            ProgressManager.checkCanceled();
+                            try (JarInputStream jis = new JarInputStream(new FileInputStream(artifact.getFile()))) {
+                                ZipEntry entry;
+                                while ((entry = jis.getNextEntry()) != null) {
+                                    if (entry.isDirectory() && entry.getName().equals(directory)) {
+                                        while ((entry = jis.getNextEntry()) != null && !entry.isDirectory()) {
+                                            String entryFileName = entry.getName().substring(entry.getName().lastIndexOf(Paths.DELIM) + 1);
+                                            String entryFileNameWithoutExtention = entryFileName.contains(".") ? entryFileName.substring(0, entryFileName.lastIndexOf('.')) : entryFileName;
+                                            if (entryFileNameWithoutExtention.startsWith(filenameWithoutExtension) &&
+                                                    (entryFileNameWithoutExtention.length() == filenameWithoutExtension.length() || entryFileNameWithoutExtention.charAt(filenameWithoutExtension.length()) == '$')) {
+                                                File out = new File(tempClassDir, entryFileName);
+                                                Files.copy(jis, out.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+                                            }
+                                        }
+                                        break;
+                                    }
+                                }
+                            } catch (IOException e) {
+                                LOGGER.error("Failed scanning and extracting matching files from jars to hotswap", e);
+                                progress.addMessage(session, MessageCategory.ERROR, "Failed finding files to hotswap");
+                            }
+                        });
+
+                Map<DebuggerSession, Map<String, HotSwapFile>> sessionMap = new HashMap<>();
+                Map<String, HotSwapFile> hotSwapFileMap = new HashMap<>();
+                sessionMap.put(session, hotSwapFileMap);
+                for (File file : tempClassDir.listFiles()) {
+                    String fileName = file.getName();
+                    String name = directory.replace(Paths.DELIM, '.') +
+                            (fileName.endsWith(".class") ?
+                                    fileName.substring(0, fileName.indexOf(".class")) :
+                                    fileName);
+                    hotSwapFileMap.put(name, new HotSwapFile(file));
+                }
+
+                ProgressManager.checkCanceled();
+                if (session.isRunning()) {
+                    HotSwapManager.reloadModifiedClasses(sessionMap, progress);
+                }
+            } finally {
+                tempClassDir.delete();
+                progress.finished();
+
+            }
+        }, progress.getProgressIndicator());
+    }
+
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        DebuggerSession session =
+                DebuggerManagerEx.getInstanceEx(e.getProject()).getContext().getDebuggerSession();
+        e.getPresentation().setEnabled(session != null);
     }
 }
