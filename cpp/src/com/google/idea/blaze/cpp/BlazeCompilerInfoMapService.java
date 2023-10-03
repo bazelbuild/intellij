ed523024afdbcec5ebb18b71273d93667c136ca8
Borja Lorente <blorente@apple.com>
2023-10-03 12:10:23 +0100

    fix(clion): Fix deserialization of BlazeCompilerInfoMap
    JetBrains' deserialization logic calls `Map.clear()`, and `ImmutableMap`
    doesn't support this.
    
    In practice, this resulted in a silent error and the state not being
    loaded properly.

--- cpp/src/com/google/idea/blaze/cpp/BlazeCompilerInfoMapService.java
+++ cpp/src/com/google/idea/blaze/cpp/BlazeCompilerInfoMapService.java
@@ -1,7 +1,6 @@
 package com.google.idea.blaze.cpp;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.idea.blaze.base.ideinfo.TargetKey;
 import com.google.idea.blaze.base.model.primitives.Label;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.RoamingType;
@@ -15,7 +14,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @State(name="BlazeCompilerInfoMap", storages = @Storage(value = "blazeInfo.xml", roamingType = RoamingType.DISABLED))
 final public class BlazeCompilerInfoMapService implements PersistentStateComponent<BlazeCompilerInfoMapService.State> {
@@ -27,35 +25,43 @@ final public class BlazeCompilerInfoMapService implements PersistentStateCompone
 
     @Override
     public void loadState(@NotNull State state) {
+        if (state.compilerVersionStringMap.isEmpty()) {
+            logger.error("Loading empty state - Some compiler flags may stop working");
+        }
         this.state = state;
     }
 
     public static class State{
         // We annotate non-public property in order to be saved using PersistentStateComponent interface
         // https://plugins.jetbrains.com/docs/intellij/persisting-state-of-components.html
+        // State cannot be an ImmutableMap, because the IDE uses Map.clear() internally,
+        // which ImmutableMap doesn't support.
         @NotNull @Property
-        ImmutableMap<String, String> compilerVersionStringMap = ImmutableMap.of();
+        Map<String, String> compilerVersionStringMap = Collections.emptyMap();
 
-        public State() {
+        public State() {}
 
-        }
-
-        public State(@NotNull ImmutableMap<String, String> compilerInfoMap) {
+        public State(@NotNull Map<String, String> compilerInfoMap) {
+            assert !(compilerInfoMap instanceof ImmutableMap);
             this.compilerVersionStringMap = compilerInfoMap;
         }
     }
 
     BlazeCompilerInfoMapService(Project p) {
-
     }
 
     public void setState(ImmutableMap<String, String> compilerInfoMap) {
-        this.state = new State(compilerInfoMap);
+        // Yes, this is inefficient and will copy the values.
+        // However, we need the map to be anything BUT an ImmutableMap in this class
+        // (see the comment on `State`'s constructor), but we want it to be an ImmutableMap everywhere else.
+        // Copying here shouldn't be a big performance hit because it happens once after every sync.
+        Map<String, String> mutableMap = Collections.checkedMap(compilerInfoMap, String.class, String.class);
+        this.state = new State(mutableMap);
     }
 
     // We annotate non-public property in order to be saved using PersistentStateComponent interface
     // https://plugins.jetbrains.com/docs/intellij/persisting-state-of-components.html
-    @Property
+    @NotNull @Property
     private State state = new State();
 
     @NotNull
@@ -64,7 +70,11 @@ final public class BlazeCompilerInfoMapService implements PersistentStateCompone
     }
 
     public boolean isClangTarget(Label label) {
-        String result = getState().compilerVersionStringMap.get(label.toString());
+        Map<String, String> compilerVersionStringMap = getState().compilerVersionStringMap;
+        if (compilerVersionStringMap.isEmpty()) {
+            logger.error("Compiler version mappings are uninitialized. This is likely because of an error de-serializing the state. Please check the logs for another error message.");
+        }
+        String result = compilerVersionStringMap.get(label.toString());
         if(result == null) {
             logger.warn(String.format("Could not find version string for target %s", label));
             return false;
