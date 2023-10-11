ba1bf5d981be855c06ad606397fb686a417fc30f
Googler <intellij-github@google.com>
2023-10-10 17:51:05 -0700

    Enable studio deployer experiment for Mac
    PiperOrigin-RevId: 572417919

--- aswb/src/com/google/idea/blaze/android/run/binary/mobileinstall/StudioDeployerExperiment.java
+++ aswb/src/com/google/idea/blaze/android/run/binary/mobileinstall/StudioDeployerExperiment.java
@@ -16,7 +16,6 @@
 package com.google.idea.blaze.android.run.binary.mobileinstall;
 
 import com.google.idea.common.experiments.FeatureRolloutExperiment;
-import com.intellij.openapi.util.SystemInfo;
 
 /**
  * A utility class that manages the experiment to use studio's built in deployer for deploying apks
@@ -29,12 +28,6 @@ public class StudioDeployerExperiment {
 
   /** Returns whether mobile install deployments should happen via the studio deployer. */
   public static boolean isEnabled() {
-    // The Studio deployer experiment is specific to local builds on Linux. For other platforms,
-    // we'll rely entirely on the new Blaze specific deployment flow.
-    if (!SystemInfo.isLinux) {
-      return false;
-    }
-
     return useStudioDeployer.isEnabled();
   }
 
