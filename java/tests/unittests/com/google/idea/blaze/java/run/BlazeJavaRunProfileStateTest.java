36a6452a46a9c12dcb0c1ffe069dd86a692cb0ec
Googler <intellij-github@google.com>
2023-11-13 12:39:09 -0800

    Support IPv4 in debugging
    (cherry picked from commit b840aa2d9511f8cee4bee4132044fb3b0037ae7d)

--- java/tests/unittests/com/google/idea/blaze/java/run/BlazeJavaRunProfileStateTest.java
+++ java/tests/unittests/com/google/idea/blaze/java/run/BlazeJavaRunProfileStateTest.java
@@ -176,7 +176,7 @@ public class BlazeJavaRunProfileStateTest extends BlazeTestCase {
                     configuration,
                     ImmutableList.of(),
                     ExecutorType.DEBUG,
-                    /*kotlinxCoroutinesJavaAgent=*/ null)
+                    /* kotlinxCoroutinesJavaAgent= */ null)
                 .build()
                 .toList())
         .isEqualTo(
@@ -185,7 +185,7 @@ public class BlazeJavaRunProfileStateTest extends BlazeTestCase {
                 "command",
                 BlazeFlags.getToolTagFlag(),
                 "--java_debug",
-                "--test_arg=--wrapper_script_flag=--debug=5005",
+                "--test_arg=--wrapper_script_flag=--debug=127.0.0.1:5005",
                 "--",
                 "//label:rule"));
   }
@@ -229,7 +229,7 @@ public class BlazeJavaRunProfileStateTest extends BlazeTestCase {
                     configuration,
                     ImmutableList.of(),
                     ExecutorType.DEBUG,
-                    /*kotlinxCoroutinesJavaAgent=*/ null)
+                    /* kotlinxCoroutinesJavaAgent= */ null)
                 .build()
                 .toList())
         .isEqualTo(
@@ -239,7 +239,7 @@ public class BlazeJavaRunProfileStateTest extends BlazeTestCase {
                 BlazeFlags.getToolTagFlag(),
                 "--",
                 "//label:java_binary_rule",
-                "--wrapper_script_flag=--debug=5005"));
+                "--wrapper_script_flag=--debug=127.0.0.1:5005"));
   }
 
   @Test
