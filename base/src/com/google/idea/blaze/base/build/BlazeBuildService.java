ebbb39f4670870b43d274a3684c9984263ac1bdd
Googler <intellij-github@google.com>
2023-11-27 09:37:27 -0800

    Support CompileFileAction for query sync
    PiperOrigin-RevId: 585677389

--- base/src/com/google/idea/blaze/base/build/BlazeBuildService.java
+++ base/src/com/google/idea/blaze/base/build/BlazeBuildService.java
@@ -15,6 +15,8 @@
  */
 package com.google.idea.blaze.base.build;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
@@ -86,6 +88,11 @@ public class BlazeBuildService {
   public BlazeBuildService(Project project) {
     this.project = project;
     this.buildSystem = Blaze.getBuildSystemProvider(project).getBuildSystem();
+  }
+
+  public void buildFileForLabels(
+      String fileName, ImmutableSet<com.google.idea.blaze.common.Label> labels) {
+    buildFile(fileName, labels.stream().map(Label::create).collect(toImmutableSet()));
   }
 
   public void buildFile(String fileName, ImmutableCollection<Label> targets) {
