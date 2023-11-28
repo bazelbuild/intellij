bc204602517bcfcfca380f7aef2aab939b62d99c
Googler <intellij-github@google.com>
2023-11-28 07:09:42 -0800

    Add support for generated in-project srcjars where the java package does not match the srcjar path.
    Make use of the existing package prefix on content entries to support this. Update `SrcJarInnerPathFinder` to support this, and add some test data too.
    
    PiperOrigin-RevId: 585965873

--- querysync/java/com/google/idea/blaze/qsync/SrcJarInnerPathFinder.java
+++ querysync/java/com/google/idea/blaze/qsync/SrcJarInnerPathFinder.java
@@ -30,15 +30,49 @@ import java.util.zip.ZipFile;
 
 /** Utility for finding inner paths of a source jar corresponding to package roots */
 public class SrcJarInnerPathFinder {
-  private final Logger logger = Logger.getLogger(SrcJarInnerPathFinder.class.getSimpleName());
-  private final PackageStatementParser packageStatementParser;
 
-  public SrcJarInnerPathFinder(PackageStatementParser packageStatementParser) {
-    this.packageStatementParser = packageStatementParser;
+  /** Indicates whether or not non-empty package prefixes are allowed. */
+  public enum AllowPackagePrefixes {
+    /**
+     * Empty package prefixes only. Files where the java package does not match the srcjar path will
+     * be ignored.
+     */
+    EMPTY_PACKAGE_PREFIXES_ONLY,
+    /** Non empty package prefixes are allowed. */
+    ALLOW_NON_EMPTY_PACKAGE_PREFIXES;
   }
 
-  public ImmutableSet<Path> findInnerJarPaths(File jarFile) {
-    Set<Path> paths = Sets.newHashSet();
+  /** Represents a path within a srcjar file, and a package prefix to use for that path. */
+  public static class JarPath {
+    public final Path path;
+    public final String packagePrefix;
+
+    JarPath(Path path, String packagePrefix) {
+      this.path = path;
+      this.packagePrefix = packagePrefix;
+    }
+
+    static JarPath create(String path, String packagePrefix) {
+      return new JarPath(Path.of(path), packagePrefix);
+    }
+
+    static JarPath create(Path path, String packagePrefix) {
+      return new JarPath(path, packagePrefix);
+    }
+  }
+
+  private final Logger logger = Logger.getLogger(SrcJarInnerPathFinder.class.getSimpleName());
+  private final PackageStatementParser packageStatementParser;
+  private final AllowPackagePrefixes allowPackagePrefixes;
+
+  public SrcJarInnerPathFinder(
+      PackageStatementParser packageStatementParser, AllowPackagePrefixes allowPackagePrefixes) {
+    this.packageStatementParser = packageStatementParser;
+    this.allowPackagePrefixes = allowPackagePrefixes;
+  }
+
+  public ImmutableSet<JarPath> findInnerJarPaths(File jarFile) {
+    Set<JarPath> paths = Sets.newHashSet();
     try {
       ZipFile zip = new ZipFile(jarFile);
       Enumeration<? extends ZipEntry> entries = zip.entries();
@@ -65,9 +99,19 @@ public class SrcJarInnerPathFinder {
           }
           if (zipPath.equals(packageAsPath)) {
             // package root is the jar file root.
-            paths.add(Path.of(""));
+            paths.add(JarPath.create("", ""));
           } else if (zipPath.endsWith(packageAsPath)) {
-            paths.add(zipPath.subpath(0, zipPath.getNameCount() - packageAsPath.getNameCount()));
+            paths.add(
+                JarPath.create(
+                    zipPath.subpath(0, zipPath.getNameCount() - packageAsPath.getNameCount()), ""));
+          } else {
+            if (allowPackagePrefixes == AllowPackagePrefixes.ALLOW_NON_EMPTY_PACKAGE_PREFIXES) {
+              paths.add(JarPath.create(zipPath, pname));
+            } else {
+              logger.log(
+                  Level.WARNING,
+                  "Java package name " + pname + " does not match srcjar path " + zipfilePath);
+            }
           }
         }
       }
@@ -76,7 +120,7 @@ public class SrcJarInnerPathFinder {
     }
     if (paths.isEmpty()) {
       // we didn't find any java/kt sources. Add the jar file root to ensure we don't ignore it.
-      paths.add(Path.of(""));
+      paths.add(JarPath.create("", ""));
     }
     return ImmutableSet.copyOf(paths);
   }
