b32d535c1002c9a9a93b1e6d7ed2ab6c328aaed3
Mai Hussien <70515749+mai93@users.noreply.github.com>
2024-01-15 16:34:52 -0800

    Do not add 2 import sections for the same project view

--- base/src/com/google/idea/blaze/base/wizard2/ui/BlazeEditProjectViewControl.java
+++ base/src/com/google/idea/blaze/base/wizard2/ui/BlazeEditProjectViewControl.java
@@ -37,7 +37,6 @@ import com.google.idea.blaze.base.projectview.section.sections.DirectorySection;
 import com.google.idea.blaze.base.projectview.section.sections.ImportSection;
 import com.google.idea.blaze.base.projectview.section.sections.Sections;
 import com.google.idea.blaze.base.projectview.section.sections.TargetSection;
-import com.google.idea.blaze.base.projectview.section.sections.TryImportSection;
 import com.google.idea.blaze.base.scope.BlazeContext;
 import com.google.idea.blaze.base.scope.OutputSink.Propagation;
 import com.google.idea.blaze.base.scope.Scope;
@@ -599,9 +598,6 @@ public final class BlazeEditProjectViewControl {
           ProjectView.builder()
               .add(
                   ScalarSection.builder(ImportSection.KEY)
-                      .set(selectProjectViewOption.getSharedProjectView()))
-              .add(
-                  ScalarSection.builder(TryImportSection.KEY)
                       .set(selectProjectViewOption.getSharedProjectView()))
               .build();
       projectViewSet =
