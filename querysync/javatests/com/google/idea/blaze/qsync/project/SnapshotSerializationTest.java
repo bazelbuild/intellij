79ca8ccb9fd6dc8a26ba5e1c70281ca83c8974d7
Googler <intellij-github@google.com>
2024-02-17 10:12:09 -0800

    Updates copybara config
    PiperOrigin-RevId: 607982494

--- querysync/javatests/com/google/idea/blaze/qsync/project/SnapshotSerializationTest.java
+++ querysync/javatests/com/google/idea/blaze/qsync/project/SnapshotSerializationTest.java
@@ -34,6 +34,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+// REPO-ONLY: import static com.google.common.truth.Truth8.assertThat;
+
 @RunWith(JUnit4.class)
 public class SnapshotSerializationTest {
 
