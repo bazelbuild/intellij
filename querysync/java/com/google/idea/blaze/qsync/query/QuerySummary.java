65c2b4dac80b33395b23b11fd9566c7ca0d2598a
Googler <intellij-github@google.com>
2024-02-29 08:07:03 -0800

    Pass label names through the Label class when parsing query output.
    This will result in the label name string being interned by the label interner, and should help reduce duplicate strings in memory.
    
    PiperOrigin-RevId: 611474378

--- querysync/java/com/google/idea/blaze/qsync/query/QuerySummary.java
+++ querysync/java/com/google/idea/blaze/qsync/query/QuerySummary.java
@@ -193,7 +193,7 @@ public abstract class QuerySummary {
               rule.setInstruments(a.getStringValue());
             }
           }
-          ruleMap.put(target.getRule().getName(), rule.build());
+          ruleMap.put(Label.of(target.getRule().getName()).toString(), rule.build());
           break;
         default:
           break;
