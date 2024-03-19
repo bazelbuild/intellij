e9dd859e29d1a598cfcd90ffe71100b09a84f8b1
Borja Lorente <blorente@apple.com>
2024-03-19 11:28:02 +0000

    Address comments

--- examples/go/with_proto/proto/translators.go
+++ examples/go/with_proto/proto/translators.go
@@ -5,7 +5,8 @@ import (
 )
 
 // Time translation functions.
-
+// If all symbols resolve correctly (i.e. there are no red squigglies after a sync)
+// then this target is working.
 func ToProtoTime(t time.Time) Time {
 	return Time{Value: t.Format(time.RFC3339Nano)}
 }
