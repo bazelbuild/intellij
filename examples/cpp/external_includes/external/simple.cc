00ebcdde70855090bb1c6c5817ead777c9ba29fc
Daniel Brauner <daniel-brauner@outlook.com>
2024-03-28 09:49:21 +0100

    Added example project

---
+++ examples/cpp/external_includes/external/simple.cc
@@ -0,0 +1,7 @@
+#include "simple.h"
+
+#include <stdio.h>
+
+void simple_header_fun() {
+	printf("Hello there!\n");
+}
---
+++ examples/cpp/external_includes/external/simple.h
@@ -0,0 +1,6 @@
+#ifndef SIMPLE_H
+#define SIMPLE_H
+
+void simple_header_fun();
+
+#endif
