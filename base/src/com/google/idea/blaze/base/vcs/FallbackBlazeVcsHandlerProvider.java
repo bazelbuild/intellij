4625e2fbbf12cd79b05a5aa76c65766654d94c1f
Mai Hussien <messa@google.com>
2024-05-30 11:43:37 -0700

    Modify `BlazeVcsHandlerProvider$handlesProject`
    to receive `Project` instead of `BuildSystemName`
    
    (cherry picked from commit 97798f68a59d0e32ded6d3a1ce0eda5613ed7f45)

--- base/src/com/google/idea/blaze/base/vcs/FallbackBlazeVcsHandlerProvider.java
+++ base/src/com/google/idea/blaze/base/vcs/FallbackBlazeVcsHandlerProvider.java
@@ -22,6 +22,7 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.idea.blaze.base.model.primitives.WorkspacePath;
 import com.google.idea.blaze.base.model.primitives.WorkspaceRoot;
 import com.google.idea.blaze.base.scope.BlazeContext;
+import com.google.idea.blaze.base.settings.Blaze;
 import com.google.idea.blaze.base.settings.BuildSystemName;
 import com.google.idea.blaze.base.sync.workspace.WorkingSet;
 import com.google.idea.blaze.common.vcs.VcsState;
@@ -42,8 +43,8 @@ public class FallbackBlazeVcsHandlerProvider implements BlazeVcsHandlerProvider 
   }
 
   @Override
-  public boolean handlesProject(BuildSystemName buildSystemName, WorkspaceRoot workspaceRoot) {
-    return buildSystemName == BuildSystemName.Bazel;
+  public boolean handlesProject(Project project, WorkspaceRoot workspaceRoot) {
+    return Blaze.getBuildSystemName(project) == BuildSystemName.Bazel;
   }
 
   @Override
