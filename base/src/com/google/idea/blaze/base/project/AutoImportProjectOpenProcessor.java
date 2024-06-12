ea890581a8d43cec098acaa73e9be9e12f645ea1
Mai Hussien <messa@google.com>
2024-06-12 10:05:29 -0700

    Internal change
    (cherry picked from commit 6c34ae52d94f4877985a23ae76468d7e090bb265)

--- base/src/com/google/idea/blaze/base/project/AutoImportProjectOpenProcessor.java
+++ base/src/com/google/idea/blaze/base/project/AutoImportProjectOpenProcessor.java
@@ -36,6 +36,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Objects;
+import java.util.Optional;
 import javax.swing.Icon;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
@@ -113,7 +114,9 @@ public class AutoImportProjectOpenProcessor extends ProjectOpenProcessor {
     }
 
     Project newProject = createProject(virtualFile);
-    Objects.requireNonNull(newProject);
+    if (newProject == null) {
+      return null;
+    }
 
     newProject.putUserData(PROJECT_AUTO_IMPORTED, true);
 
@@ -170,12 +173,13 @@ public class AutoImportProjectOpenProcessor extends ProjectOpenProcessor {
       LOG.error("Failed to commit project import builder", e);
     }
 
-    Project newProject = builder.createProject(name, projectFilePath);
-    if (newProject == null) {
-      LOG.error("Failed to Bazel create project");
+    Optional<Project> returnedValue = ExtendableBazelProjectCreator.getInstance()
+        .createProject(builder, name, projectFilePath);
+    if (returnedValue.isEmpty()) {
       return null;
     }
-
+	
+    Project newProject = returnedValue.get();
     newProject.save();
 
     if (!builder.validate(null, newProject)) {
