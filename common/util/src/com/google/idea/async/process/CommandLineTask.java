933664ca4bc42121b2f8c86d55835be0546d3fce
Daniel Brauner <44034965+LeFrosch@users.noreply.github.com>
2024-07-11 16:21:35 +0200

    Fixes after sync freeze on MacOS  (#6562)
    * Removed read action for BlazeCWorkspace update
    
    * Added assertion to ensure command line tasks do not cause a freeze
    
    ---------
    
    Co-authored-by: Evgenii Novozhilov <evgenii.novozhilov@jetbrains.com>

--- common/util/src/com/google/idea/async/process/CommandLineTask.java
+++ common/util/src/com/google/idea/async/process/CommandLineTask.java
@@ -23,9 +23,11 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Maps;
 import com.google.common.io.ByteStreams;
 import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.EnvironmentUtil;
 import com.intellij.util.execution.ParametersListUtil;
+import com.intellij.util.ui.EDT;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -181,6 +183,11 @@ public interface CommandLineTask {
 
     @Override
     public int run() throws IOException, InterruptedException, TimeoutException {
+
+      // ensure execution on background thread
+      assert !EDT.isCurrentThreadEdt();
+      // ensure no read lock is kept
+      assert !ApplicationManager.getApplication().isReadAccessAllowed();
 
       String logCommand = ParametersListUtil.join(command);
       if (logCommand.length() > 2000) {
