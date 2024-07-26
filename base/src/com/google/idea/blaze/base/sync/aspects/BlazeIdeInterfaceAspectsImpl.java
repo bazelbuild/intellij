7083d74a09d22e2013b8b56dfb19d9180d893e4f
Mauricio Galindo <mauricio.galindo@snowflake.com>
2024-07-26 11:10:24 -0700

    Update BlazeIdeInterfaceAspectsImpl.java

--- base/src/com/google/idea/blaze/base/sync/aspects/BlazeIdeInterfaceAspectsImpl.java
+++ base/src/com/google/idea/blaze/base/sync/aspects/BlazeIdeInterfaceAspectsImpl.java
@@ -106,7 +106,6 @@ import java.io.IOException;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.time.Instant;
 import java.util.ArrayList;
