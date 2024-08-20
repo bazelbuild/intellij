1bfda0e1b65f2d79d398f011466a9e71599aa2be
Chao Cheng <chao.cheng@compass.com>
2024-08-20 19:44:42 +0800

    fix go test not found problem (#6634)
    * fix go test not found problem
    
    * fix tests
    
    * only apply -m for testify.
    
    * add tests.

--- golang/src/com/google/idea/blaze/golang/run/producers/GoTestContextProvider.java
+++ golang/src/com/google/idea/blaze/golang/run/producers/GoTestContextProvider.java
@@ -15,12 +15,17 @@
  */
 package com.google.idea.blaze.golang.run.producers;
 
+import javax.annotation.Nullable;
+
 import com.goide.execution.testing.GoTestFinder;
 import com.goide.execution.testing.GoTestRunConfigurationProducerBase;
+import com.goide.execution.testing.frameworks.testify.GoTestifySupport;
 import com.goide.psi.GoFile;
 import com.goide.psi.GoFunctionOrMethodDeclaration;
+import com.goide.psi.GoMethodDeclaration;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.idea.blaze.base.command.BlazeFlags;
 import com.google.idea.blaze.base.dependencies.TargetInfo;
 import com.google.idea.blaze.base.run.ExecutorType;
 import com.google.idea.blaze.base.run.TestTargetHeuristic;
@@ -30,7 +35,6 @@ import com.google.idea.blaze.base.run.producers.TestContextProvider;
 import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import javax.annotation.Nullable;
 
 class GoTestContextProvider implements TestContextProvider {
 
@@ -110,12 +114,20 @@ class GoTestContextProvider implements TestContextProvider {
           .setDescription(file.getName())
           .build();
     }
+
     String testFilterRegex = regexifyTestFilter(calculateRawTestFilterForElement(element, function));
-    return TestContext.builder(/* sourceElement= */ function, ExecutorType.DEBUG_SUPPORTED_TYPES)
+    TestContext.Builder builder = TestContext.builder(/* sourceElement= */ function,
+            ExecutorType.DEBUG_SUPPORTED_TYPES)
         .addTestEnv(GO_TEST_WRAP_TESTV)
         .setTarget(target)
-        .setTestFilter(testFilterRegex)
-        .setDescription(String.format("%s#%s", file.getName(), function.getName()))
-        .build();
+        .setDescription(String.format("%s#%s", file.getName(), function.getName()));
+    if (function instanceof GoMethodDeclaration method &&
+        GoTestifySupport.getTestifySuiteTypeSpec(method) != null) {
+      builder.addBlazeFlagsModification(TestContext.BlazeFlagsModification.addFlagIfNotPresent(
+          BlazeFlags.TEST_ARG + "-testify.m=" + testFilterRegex));
+    } else {
+      builder.setTestFilter(testFilterRegex);
+    }
+    return builder.build();
   }
 }
