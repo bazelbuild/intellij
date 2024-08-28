628a105cdbfe6fe21bdb62aeb096e8eec2fec579
Mihai Claudiu Toader <mihai@sigmacomputing.com>
2024-08-28 01:40:13 -0700

    [#6664] Add basic model for ExternalWorkspaces - 1/n (#6669)
    * [#6664] Add basic model for ExternalWorkspaces - 1/n
    
    * Update MockProjectDataBuilder
    
    * Fix code style
    
    * Use `Functions.identity()`
    
    * Add rest of the dependencies
    
    * Avoid some unnecessary changes
    
    * Avoid some unnecessary changes

---
+++ base/src/com/google/idea/blaze/base/model/ExternalWorkspaceData.java
@@ -0,0 +1,46 @@
+package com.google.idea.blaze.base.model;
+
+import com.google.common.base.Functions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.devtools.intellij.model.ProjectData;
+import com.google.idea.blaze.base.ideinfo.ProtoWrapper;
+import com.google.idea.blaze.base.model.primitives.ExternalWorkspace;
+
+public final class ExternalWorkspaceData implements ProtoWrapper<ProjectData.ExternalWorkspaceData> {
+  public ImmutableMap<String, ExternalWorkspace> workspaces;
+
+  public static ExternalWorkspaceData EMPTY = new ExternalWorkspaceData(ImmutableList.of());
+
+  public static ExternalWorkspaceData create(ImmutableList<ExternalWorkspace> workspaces) {
+    return new ExternalWorkspaceData(workspaces);
+  }
+
+  ExternalWorkspaceData(ImmutableList<ExternalWorkspace> workspaces) {
+    this.workspaces = ImmutableMap.copyOf(
+        workspaces
+            .stream()
+            .collect(
+                ImmutableMap.toImmutableMap(
+                    ExternalWorkspace::name,
+                    Functions.identity()))
+    );
+  }
+
+  @Override
+  public ProjectData.ExternalWorkspaceData toProto() {
+    ImmutableList<ProjectData.ExternalWorkspace> protoWorkspaces = workspaces
+        .values()
+        .stream()
+        .map(ExternalWorkspace::toProto)
+        .collect(ImmutableList.toImmutableList());
+
+    return ProjectData.ExternalWorkspaceData.newBuilder()
+        .addAllWorkspaces(protoWorkspaces)
+        .build();
+  }
+
+  public static ExternalWorkspaceData fromProto(ProjectData.ExternalWorkspaceData proto) {
+    return new ExternalWorkspaceData(proto.getWorkspacesList().stream().map(ExternalWorkspace::fromProto).collect(ImmutableList.toImmutableList()));
+  }
+}
