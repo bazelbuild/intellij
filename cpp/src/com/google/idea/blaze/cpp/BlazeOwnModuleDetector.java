e4383d0985f6984d6922d187f454b5836dec07fc
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2024-09-05 14:51:33 +0200

    chore: drop 222: Inline CidrOwnModuleDetectorWrapper (#6732)

--- cpp/src/com/google/idea/blaze/cpp/BlazeOwnModuleDetector.java
+++ cpp/src/com/google/idea/blaze/cpp/BlazeOwnModuleDetector.java
@@ -1,12 +1,12 @@
 package com.google.idea.blaze.cpp;
 
-import com.google.idea.sdkcompat.cpp.CidrOwnModuleDetectorWrapper;
 import com.intellij.openapi.module.Module;
+import com.jetbrains.cidr.project.workspace.CidrOwnModuleDetector;
 import org.jetbrains.annotations.NotNull;
 
 import static com.google.idea.blaze.base.settings.Blaze.isBlazeProject;
 
-public class BlazeOwnModuleDetector implements CidrOwnModuleDetectorWrapper {
+public class BlazeOwnModuleDetector implements CidrOwnModuleDetector {
     @Override
     public boolean isOwnModule(@NotNull Module module) {
         return isBlazeProject(module.getProject());
