46277d1187eaf7d3a7f2f594280b3d8391243bd9
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2024-09-23 22:07:36 +0200

    chore: Prepare for 243 bump: move addListeners to SDK compat (#6781)

---
+++ sdkcompat/v243/com/google/idea/sdkcompat/general/AsyncVfsEventsPostProcessorCompat.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2024 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.idea.sdkcompat.general;
+
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.components.ComponentManagerEx;
+import com.intellij.openapi.project.Project;
+import com.intellij.vfs.AsyncVfsEventsListener;
+import com.intellij.vfs.AsyncVfsEventsPostProcessor;
+
+// #api242
+public class AsyncVfsEventsPostProcessorCompat {
+    private AsyncVfsEventsPostProcessorCompat(){
+
+    }
+
+    public static void addListener(AsyncVfsEventsListener listener, Disposable disposable, Project project) {
+        AsyncVfsEventsPostProcessor.getInstance().addListener(listener, ((ComponentManagerEx)project).getCoroutineScope());
+    }
+}
