481c96adaf5469c423c16585e841961879014b5f
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2024-10-08 14:38:14 +0200

    Disable bazel deps call. It's unused yet, and could fail (#6860)
    * Disable bazel deps call. It's unused yet, and could fail
    
    https://github.com/bazelbuild/bazel/issues/22691

--- base/src/com/google/idea/blaze/base/model/ExternalWorkspaceDataProvider.java
+++ base/src/com/google/idea/blaze/base/model/ExternalWorkspaceDataProvider.java
@@ -46,7 +46,6 @@ public class ExternalWorkspaceDataProvider {
   private final Project project;
 
   private volatile ExternalWorkspaceData externalWorkspaceData;
-  private ListenableFuture<String> deps;
 
   public ExternalWorkspaceDataProvider(Project project) {
     this.project = project;
@@ -105,13 +104,11 @@ public class ExternalWorkspaceDataProvider {
               .getBuildSystem()
               .getDefaultInvoker(project, context);
 
-      deps = BlazeModRunner.getInstance().getDeps(project, buildInvoker, context, importSettings.getBuildSystem(), blazeFlags);
       externalWorkspaceData =
           BlazeModRunner.getInstance()
               .dumpRepoMapping(
                   project, buildInvoker, context, importSettings.getBuildSystem(), blazeFlags)
               .get();
-      deps.get();
     } catch (InterruptedException | ExecutionException e) {
       context.handleExceptionAsWarning(
           "Failed to run `blaze mod dump_repo_mapping` (completion of labels from module provided repos will be unavailable)",
