839a6720a623898604a86b498e04c5f8ffbcc2ea
Evgenii Novozhilov <evgenii.novozhilov@jetbrains.com>
2024-10-16 19:27:15 +0300

    Decorate tabs for BUILD files with package name
    Since BUILD files all have the same name it's hard
    to distinguish them, so we can use EditorTabTitleProvider
    to write full label in the editor tab title

---
+++ base/src/com/google/idea/blaze/base/editor/BazelEditorTabTitleProvider.java
@@ -0,0 +1,31 @@
+package com.google.idea.blaze.base.editor;
+
+import com.google.idea.blaze.base.lang.buildfile.psi.BuildFile;
+import com.google.idea.blaze.base.settings.Blaze;
+import com.intellij.openapi.fileEditor.impl.EditorTabTitleProvider;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class BazelEditorTabTitleProvider implements EditorTabTitleProvider {
+  private static boolean requiresDecoration(Project project, String fileName) {
+    return Blaze.getBuildSystemProvider(project).possibleBuildFileNames().contains(fileName);
+  }
+  @Override
+  public @Nullable String getEditorTabTitle(@NotNull Project project,
+      @NotNull VirtualFile virtualFile) {
+    var fileName = virtualFile.getName();
+    if (!requiresDecoration(project, fileName)) {
+      return null;
+    }
+
+    return BuildFile.getBuildFileString(project, virtualFile.getPath());
+  }
+
+  @Override
+  public @Nullable String getEditorTabTooltipText(@NotNull Project project,
+      @NotNull VirtualFile virtualFile) {
+    return EditorTabTitleProvider.super.getEditorTabTooltipText(project, virtualFile);
+  }
+}
