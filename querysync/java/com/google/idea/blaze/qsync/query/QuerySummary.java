10c419ae946247cb6e2ac0a52f1f393cfad8fc06
Googler <intellij-github@google.com>
2024-11-05 11:06:33 +0100

    Do not validate/normalize labels on load
    since they have been validated and normalized before being serialized.
    
    Bug: 370729642
    Test: n/a
    Change-Id: I4a49ae7ba2021a400d6634cea59eddc3f2e87173
    
    AOSP: cb3f07cd985b75d7c56f8f42796dca709da95e6b

--- querysync/java/com/google/idea/blaze/qsync/query/QuerySummary.java
+++ querysync/java/com/google/idea/blaze/qsync/query/QuerySummary.java
@@ -374,7 +374,7 @@ public abstract class QuerySummary {
     return proto().getStoredRulesMap().entrySet().stream()
         .collect(
             toImmutableMap(
-                e -> Label.of(lookup.lookup(e.getKey())),
+                e -> Label.createLabelWithoutValidation(lookup.lookup(e.getKey())),
                 t -> lookup.storedRuleToRule(t.getValue())));
   }
 
