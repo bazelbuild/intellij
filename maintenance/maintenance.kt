6cedf46dd3610f2f03b4f52c7a475d9a176b1082
Tomasz Pasternak <tomasz.pasternak@jetbrains.com>
2024-11-14 07:57:35 +0100

    Update maintenance.kt

--- maintenance/maintenance.kt
+++ maintenance/maintenance.kt
@@ -23,7 +23,7 @@ fun main(args: Array<String>) { // run with WORKSPACE file path as the first arg
     bumpPlugins("241", out)
     bumpRelease("2024.2", "242", out)
     bumpPlugins("242", out)
-    bumpEap("243", out)
+    bumpRelease("2024.3", "243", out)
     bumpPlugins("243", out)
     bumpMavenPackages("junit:junit", "JUNIT", out)
 }
