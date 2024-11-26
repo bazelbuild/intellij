b2e2de7d56b04e995d47ad75f6aa00ea2a9b2290
Xuan-Son Trinh <xuanson.trinh@jetbrains.com>
2024-11-26 16:47:46 +0100

    implement `PluginReplacement` to point to the new Bazel plugin

---
+++ base/src/com/google/idea/blaze/base/plugin/NewBazelPluginReplacement.java
@@ -0,0 +1,9 @@
+package com.google.idea.blaze.base.plugin;
+
+import com.intellij.ide.plugins.PluginReplacement;
+
+public class NewBazelPluginReplacement extends PluginReplacement {
+    public NewBazelPluginReplacement() {
+        super("org.jetbrains.bazel");
+    }
+}
