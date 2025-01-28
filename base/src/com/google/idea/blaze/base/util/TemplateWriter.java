c475f399875cd19d555c40d46c11448cc1391c97
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-01-28 14:24:30 +0100

    replace vfs with a manifest file
    Since vfs is not requires some hacks with to refresh files and might
    break in future it should be avoided. Further more, vfs is not available
    in some tests and this way logic for copying aspects can be reused.

--- base/src/com/google/idea/blaze/base/util/TemplateWriter.java
+++ base/src/com/google/idea/blaze/base/util/TemplateWriter.java
@@ -1,5 +1,6 @@
 package com.google.idea.blaze.base.util;
 
+import com.google.idea.blaze.base.sync.aspects.storage.AspectWriter;
 import com.intellij.openapi.vfs.VirtualFile;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -17,7 +18,7 @@ public class TemplateWriter {
   public static void evaluate(
       Path destinationDirectory,
       String destinationFile,
-      VirtualFile templateDirectory,
+      String templateDirectory,
       String templateFile,
       Map<String, ?> options
   ) throws IOException {
@@ -25,22 +26,19 @@ public class TemplateWriter {
       Files.createDirectories(destinationDirectory);
     }
 
+    final var template = AspectWriter.readAspect(TemplateWriter.class, templateDirectory, templateFile);
+
     final var dstStream = Files.newOutputStream(
         destinationDirectory.resolve(destinationFile),
         StandardOpenOption.CREATE,
         StandardOpenOption.TRUNCATE_EXISTING
     );
 
-    final var template = templateDirectory.findChild(templateFile);
-    if (template == null || !template.isValid()) {
-      throw new IOException("Could not find valid template file: " + templateFile);
-    }
-
     final var ctx = new VelocityContext();
     options.forEach(ctx::put);
 
     try (final var writer = new OutputStreamWriter(dstStream)) {
-      try (final var reader = new InputStreamReader(template.getInputStream())) {
+      try (final var reader = new InputStreamReader(template.openStream())) {
         final var success = Velocity.evaluate(ctx, writer, templateFile, reader);
 
         if (!success) {
