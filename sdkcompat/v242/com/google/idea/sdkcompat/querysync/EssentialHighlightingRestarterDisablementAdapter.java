1bb2cd6c1ecb92ecea75e68177e400ee88a6975f
Błażej Kardyś <blazej.kardys@jetbrains.com>
2025-02-11 17:00:07 +0100

    Adding sdkcompat for 251

--- sdkcompat/v242/com/google/idea/sdkcompat/querysync/EssentialHighlightingRestarterDisablementAdapter.java
+++ sdkcompat/v242/com/google/idea/sdkcompat/querysync/EssentialHighlightingRestarterDisablementAdapter.java
@@ -5,6 +5,5 @@ import org.jetbrains.annotations.NotNull;
 
 public interface EssentialHighlightingRestarterDisablementAdapter {
 
-  /* api243 */
   public boolean shouldBeDisabledForProject(@NotNull Project project);
 }
