05769882ad69348ad1ce5a83b65c205c6fdd841b
Błażej Kardyś <blazej.kardys@jetbrains.com>
2025-02-14 14:21:43 +0100

    Review fixes

--- sdkcompat/v242/com/google/idea/sdkcompat/querysync/EssentialHighlightingRestarterDisablementAdapter.java
+++ sdkcompat/v242/com/google/idea/sdkcompat/querysync/EssentialHighlightingRestarterDisablementAdapter.java
@@ -3,6 +3,7 @@ package com.google.idea.sdkcompat.querysync;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
+// #api242
 public interface EssentialHighlightingRestarterDisablementAdapter {
 
   public boolean shouldBeDisabledForProject(@NotNull Project project);
