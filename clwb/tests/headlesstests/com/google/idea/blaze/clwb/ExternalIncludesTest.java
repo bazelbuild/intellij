2623c7a183867c1cf56da4f544b611aecfd3b4b2
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-02-21 16:19:23 +0100

    moved shared test definitions to com.google.idea.testing.headless

--- clwb/tests/headlesstests/com/google/idea/blaze/clwb/ExternalIncludesTest.java
+++ clwb/tests/headlesstests/com/google/idea/blaze/clwb/ExternalIncludesTest.java
@@ -1,21 +1,16 @@
 package com.google.idea.blaze.clwb;
 
-import static com.google.common.truth.Truth.assertThat;
 import static com.google.idea.blaze.clwb.base.Assertions.assertContainsHeader;
 
-import com.google.idea.blaze.clwb.base.BazelVersionRule;
-import com.google.idea.blaze.clwb.base.ClwbIntegrationTestCase;
-import com.intellij.openapi.util.SystemInfo;
-import com.jetbrains.cidr.lang.workspace.compiler.ClangCompilerKind;
-import com.jetbrains.cidr.lang.workspace.compiler.GCCCompilerKind;
-import com.jetbrains.cidr.lang.workspace.compiler.MSVCCompilerKind;
+import com.google.idea.blaze.clwb.base.ClwbHeadlessTestCase;
+import com.google.idea.testing.headless.BazelVersionRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class ExternalIncludesTest extends ClwbIntegrationTestCase {
+public class ExternalIncludesTest extends ClwbHeadlessTestCase {
 
   // CompilationContext.external_includes was added in bazel 7
   @Rule
