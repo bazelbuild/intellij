fa653149887552fe06a8deff902d20128edfceb9
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-02-24 10:54:56 +0100

    remove jdks on tear down

--- ijwb/tests/headlesstests/com/google/idea/blaze/ijwb/headless/base/IjwbHeadlessTestCase.java
+++ ijwb/tests/headlesstests/com/google/idea/blaze/ijwb/headless/base/IjwbHeadlessTestCase.java
@@ -1,6 +1,21 @@
 package com.google.idea.blaze.ijwb.headless.base;
 
 import com.google.idea.testing.headless.HeadlessTestCase;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.projectRoots.ProjectJdkTable;
 
 public class IjwbHeadlessTestCase extends HeadlessTestCase {
+
+  @Override
+  protected void tearDown() throws Exception {
+    final var jdkTable = ProjectJdkTable.getInstance();
+
+    WriteAction.runAndWait(() -> {
+      for (final var jdk : jdkTable.getAllJdks()) {
+        jdkTable.removeJdk(jdk);
+      }
+    });
+
+    super.tearDown();
+  }
 }
