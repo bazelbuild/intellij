e8a5e2e3f28766725327ad48ff9c8c6d01ed722a
Kanglei Fang <kanglei.fang@snowflake.com>
2025-04-02 05:26:19 +0000

    Set standalone test flags

--- base/src/com/google/idea/blaze/base/run/confighandler/BlazeCommandGenericRunConfigurationRunner.java
+++ base/src/com/google/idea/blaze/base/run/confighandler/BlazeCommandGenericRunConfigurationRunner.java
@@ -284,6 +284,30 @@ public final class BlazeCommandGenericRunConfigurationRunner
       return processHandler;
     }
 
+    private void addStandaloneRunFlagsIfSingleTestTriggered(BlazeCommand.Builder blazeCommandBuilder) {
+      // Check that only a single target is specified.
+      if (configuration.getTargets().size() != 1) {
+        return;
+      }
+
+      // Check that the target is either a unit or an IT test.
+      final var target = configuration.getTargets().get(0).toString();
+      final var pattern = "^\\/\\/[\\w\\-/]+:[\\w\\-_]+(?:Test|TestIT)$";
+      if (target.matches(pattern)) {
+        // Override the flags in not yet set explicitly.
+        final var args = blazeCommandBuilder.build().toString();
+        if (!args.contains("--test_output")) {
+          blazeCommandBuilder.addBlazeFlags("--test_output=streamed");
+        }
+        if (!args.contains("--strategy")) {
+          blazeCommandBuilder.addBlazeFlags("--strategy=TestRunner=standalone");
+        }
+        if (!args.contains("--cache_test_results")) {
+          blazeCommandBuilder.addBlazeFlags("--cache_test_results=no");
+        }
+      }
+    }
+
     private ProcessHandler getProcessHandlerForTests(
         Project project,
         BuildInvoker invoker,
@@ -292,6 +316,8 @@ public final class BlazeCommandGenericRunConfigurationRunner
         WorkspaceRoot workspaceRoot,
         BlazeContext context)
         throws ExecutionException {
+      addStandaloneRunFlagsIfSingleTestTriggered(blazeCommandBuilder);
+
       BlazeTestResultFinderStrategy testResultFinderStrategy =
           !invoker.getCommandRunner().canUseCli()
               ? new BlazeTestResultHolder()
