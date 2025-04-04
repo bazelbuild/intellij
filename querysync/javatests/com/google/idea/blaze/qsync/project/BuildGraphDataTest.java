d5488e449c1d929a312467cb4892ae20a2e31013
Błażej Kardyś <blazej.kardys@jetbrains.com>
2025-04-04 14:54:20 +0200

    Clean up QuerySyncLanguages (#7550)
    1. Query sync does not support Kotlin as a separate language. All JVM
       languages including the Android platform are either supported or not.
    
       Availability of different Bazel rule sets will need to be configured
       separately.
    
    2. Rename `JAVA` to `JVM` as it is a better description of the enum
       constant.
    
    Bug: 377525094
    Test: n/a
    Change-Id: Ia3b608777b690356519660a2f888b4d9223884b5
    
    AOSP: 55d2bec8b07eca942186456431ace5166c24d268
    
    Co-authored-by: Googler <intellij-github@google.com>

--- querysync/javatests/com/google/idea/blaze/qsync/project/BuildGraphDataTest.java
+++ querysync/javatests/com/google/idea/blaze/qsync/project/BuildGraphDataTest.java
@@ -130,7 +130,7 @@ public class BuildGraphDataTest {
     assertThat(graph.getExternalDependencies(ImmutableList.of(Label.of("//" + TESTDATA_ROOT + "/nodeps:nodeps"))))
         .isEmpty();
     assertThat(graph.targetMap().get(Label.fromWorkspacePackageAndName("", expectedPackagePath, "nodeps")).languages())
-        .containsExactly(QuerySyncLanguage.JAVA);
+        .containsExactly(QuerySyncLanguage.JVM);
   }
 
   @Test
