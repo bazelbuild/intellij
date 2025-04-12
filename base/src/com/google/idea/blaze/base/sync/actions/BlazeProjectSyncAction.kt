c4c187b33cd243a6b78183a17ecc78e583ced4fc
Evgenii Novozhilov <evgenii.novozhilov@jetbrains.com>
2025-04-12 18:51:08 +0300

    Execute sync actions in the background
    after #7088 runSync can reparse project view file
    and reparsing requires background thread because of
    git execution under the hood

--- base/src/com/google/idea/blaze/base/sync/actions/BlazeProjectSyncAction.kt
+++ base/src/com/google/idea/blaze/base/sync/actions/BlazeProjectSyncAction.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 The Bazel Authors. All rights reserved.
+ * Copyright 2025 The Bazel Authors. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,35 +13,43 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.idea.blaze.base.sync.actions;
+package com.google.idea.blaze.base.sync.actions
 
-import com.google.idea.blaze.base.actions.BlazeProjectAction;
-import com.google.idea.blaze.base.sync.status.BlazeSyncStatus;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.project.DumbAware;
-import com.intellij.openapi.project.Project;
+import com.google.idea.blaze.base.actions.BlazeProjectAction
+import com.google.idea.blaze.base.sync.status.BlazeSyncStatus
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.progress.currentThreadCoroutineScope
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-/** Base class for sync actions. */
-public abstract class BlazeProjectSyncAction extends BlazeProjectAction implements DumbAware {
+/** Base class for sync actions.  */
+abstract class BlazeProjectSyncAction : BlazeProjectAction(), DumbAware {
+  protected abstract fun runSync(project: Project?, e: AnActionEvent?)
 
-  protected abstract void runSync(Project project, AnActionEvent e);
+  override fun actionPerformedInBlazeProject(project: Project, e: AnActionEvent) {
+    currentThreadCoroutineScope().launch(Dispatchers.Default) {
+      if (!BlazeSyncStatus.getInstance(project).syncInProgress()) {
+        runSync(project, e)
+      }
 
-  @Override
-  protected void actionPerformedInBlazeProject(Project project, AnActionEvent e) {
-    if (!BlazeSyncStatus.getInstance(project).syncInProgress()) {
-      runSync(project, e);
+      withContext(Dispatchers.EDT) {
+        updateStatus(project, e)
+      }
     }
-    updateStatus(project, e);
   }
 
-  @Override
-  protected void updateForBlazeProject(Project project, AnActionEvent e) {
-    updateStatus(project, e);
+  override fun updateForBlazeProject(project: Project, e: AnActionEvent) {
+    updateStatus(project, e)
   }
 
-  private static void updateStatus(Project project, AnActionEvent e) {
-    Presentation presentation = e.getPresentation();
-    presentation.setEnabled(!BlazeSyncStatus.getInstance(project).syncInProgress());
+  companion object {
+    private fun updateStatus(project: Project, e: AnActionEvent) {
+      val presentation = e.presentation
+      presentation.isEnabled = !BlazeSyncStatus.getInstance(project).syncInProgress()
+    }
   }
 }
