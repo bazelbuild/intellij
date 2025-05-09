eaa7bf1ead508e927885a74b622877e09e07ff22
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-05-09 11:31:43 +0200

    move CcAspectTemplateWriter to base

--- base/src/com/google/idea/blaze/base/sync/aspects/storage/CcAspectTemplateWriter.kt
+++ base/src/com/google/idea/blaze/base/sync/aspects/storage/CcAspectTemplateWriter.kt
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.idea.blaze.cpp.sync
+package com.google.idea.blaze.base.sync.aspects.storage
 
 import com.google.idea.blaze.base.sync.aspects.storage.AspectRepositoryProvider.ASPECT_TEMPLATE_DIRECTORY
-import com.google.idea.blaze.base.sync.aspects.storage.AspectWriter
 import com.google.idea.blaze.base.util.TemplateWriter
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.registry.Registry
