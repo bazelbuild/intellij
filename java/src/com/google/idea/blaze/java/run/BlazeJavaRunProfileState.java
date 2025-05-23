728f45cf90805597b80d2417afa9a70e593d9d53
Evgenii Novozhilov <evgenii.novozhilov@jetbrains.com>
2025-05-23 18:51:51 +0300

    java: fix broken debugger output
    partially revert AOSP changes to
    the previous implementation
    
    fixes #7682

--- java/src/com/google/idea/blaze/java/run/BlazeJavaRunProfileState.java
+++ java/src/com/google/idea/blaze/java/run/BlazeJavaRunProfileState.java
@@ -33,6 +33,7 @@ import com.google.idea.blaze.base.command.BlazeCommandName;
 import com.google.idea.blaze.base.command.BlazeCommandRunnerExperiments;
 import com.google.idea.blaze.base.command.BlazeFlags;
 import com.google.idea.blaze.base.command.BlazeInvocationContext;
+import com.google.idea.blaze.base.command.buildresult.BuildResultHelperBep;
 import com.google.idea.blaze.base.command.buildresult.BuildResultParser;
 import com.google.idea.blaze.base.command.buildresult.bepparser.BuildEventStreamProvider;
 import com.google.idea.blaze.base.console.BlazeConsoleLineProcessorProvider;
@@ -54,6 +55,7 @@ import com.google.idea.blaze.base.run.state.BlazeCommandRunConfigurationCommonSt
 import com.google.idea.blaze.base.run.testlogs.BlazeTestResultFinderStrategy;
 import com.google.idea.blaze.base.run.testlogs.BlazeTestResultHolder;
 import com.google.idea.blaze.base.run.testlogs.BlazeTestResults;
+import com.google.idea.blaze.base.run.testlogs.LocalBuildEventProtocolTestFinderStrategy;
 import com.google.idea.blaze.base.scope.BlazeContext;
 import com.google.idea.blaze.base.scope.OutputSink;
 import com.google.idea.blaze.base.scope.scopes.IdeaLogScope;
@@ -62,6 +64,8 @@ import com.google.idea.blaze.base.settings.Blaze;
 import com.google.idea.blaze.base.settings.BlazeUserSettings;
 import com.google.idea.blaze.common.PrintOutput;
 import com.google.idea.blaze.java.TargetKindUtil;
+import com.google.idea.blaze.java.run.hotswap.HotSwapCommandBuilder;
+import com.google.idea.blaze.java.run.hotswap.HotSwapUtils;
 import com.intellij.execution.DefaultExecutionResult;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.ExecutionResult;
@@ -82,10 +86,10 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtilRt;
 import java.io.File;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import javax.annotation.Nullable;
 import org.jetbrains.annotations.NotNull;
 
@@ -129,7 +133,7 @@ public final class BlazeJavaRunProfileState extends BlazeJavaDebuggableRunProfil
         && !invoker.getCapabilities().contains(BuildInvoker.Capability.SUPPORT_CLI)) {
       return startProcessRunfilesCase(project);
     }
-    return startProcessBazelCliCase(invoker, project, context);
+    return startProcessBazelCliCase(project, context);
   }
 
   private ProcessHandler startProcessRunfilesCase(Project project) throws ExecutionException {
@@ -164,10 +168,12 @@ public final class BlazeJavaRunProfileState extends BlazeJavaDebuggableRunProfil
   }
 
   private ProcessHandler startProcessBazelCliCase(
-    BuildInvoker invoker, Project project, BlazeContext context) {
+    Project project, BlazeContext context) throws ExecutionException {
     BlazeCommand.Builder blazeCommand;
     BlazeTestUiSession testUiSession = null;
-    BlazeTestResultFinderStrategy testResultFinderStrategy = new BlazeTestResultHolder();
+    // TODO: find proper place to close the result helper (same for BlazeCidrLauncher)
+    final var buildResultHelper = new BuildResultHelperBep();
+    BlazeTestResultFinderStrategy testResultFinderStrategy = new LocalBuildEventProtocolTestFinderStrategy(buildResultHelper.getOutputFile());
     if (useTestUi()
         && BlazeTestEventsHandler.targetsSupported(project, getConfiguration().getTargets())) {
       testUiSession =
@@ -175,6 +181,7 @@ public final class BlazeJavaRunProfileState extends BlazeJavaDebuggableRunProfil
           ImmutableList.<String>builder()
             .add("--runs_per_test=1")
             .add("--flaky_test_attempts=1")
+            .addAll(buildResultHelper.getBuildFlags())
             .build(),
           testResultFinderStrategy);
     }
@@ -211,8 +218,19 @@ public final class BlazeJavaRunProfileState extends BlazeJavaDebuggableRunProfil
         WorkspaceRoot.fromProject(project),
         BlazeInvocationContext.ContextType.RunConfiguration));
 
-    //TODO: b/399392908 - Revisit code hot-swapping using HotSwapCommandBuilder
-    return getCommandRunnerProcessHandler(invoker, blazeCommand, testResultFinderStrategy, context);
+    ImmutableList<String> command;
+    if (HotSwapUtils.canHotSwap(getEnvironment())) {
+      try {
+        command = HotSwapCommandBuilder.getBashCommandsToRunScript(project, blazeCommand);
+      } catch (IOException e) {
+        logger.warn("Failed to create script path. Hot swap will be disabled.", e);
+        command = blazeCommand.build().toList();
+      }
+    } else {
+      command = blazeCommand.build().toList();
+    }
+
+    return getScopedProcessHandler(project, command, WorkspaceRoot.fromProject(project));
   }
 
   @Override
