62ece0b79f011f6b14f1ca4de3b85efd908cb654
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-08-11 13:39:00 +0200

    fixed aspect integration tests

--- aspect/testing/tests/src/com/google/idea/blaze/aspect/cpp/coptsmakevars/CoptsMakeVarsTest.java
+++ aspect/testing/tests/src/com/google/idea/blaze/aspect/cpp/coptsmakevars/CoptsMakeVarsTest.java
@@ -37,12 +37,12 @@ public class CoptsMakeVarsTest extends BazelIntellijAspectTest {
     assertThat(target.hasCIdeInfo()).isTrue();
     assertThat(target.hasJavaIdeInfo()).isFalse();
     assertThat(target.hasAndroidIdeInfo()).isFalse();
-    CIdeInfo cTargetIdeInfo = target.getCIdeInfo();
+    final var ruleContext = target.getCIdeInfo().getRuleContext();
 
-    assertThat(cTargetIdeInfo.getTargetCoptList()).hasSize(2);
+    assertThat(ruleContext.getCoptsList()).hasSize(2);
     // These predefined variables' values are dependent on build system and configuration.
-    assertThat(cTargetIdeInfo.getTargetCoptList().get(0))
+    assertThat(ruleContext.getCoptsList().get(0))
         .containsMatch("^-DPREFINED_BINDIR=(blaze|bazel)-out/[0-9a-z_-]+/bin$");
-    assertThat(cTargetIdeInfo.getTargetCoptList().get(1)).isEqualTo("-DPREFINED_BINDIR2=$(BINDIR)");
+    assertThat(ruleContext.getCoptsList().get(1)).isEqualTo("-DPREFINED_BINDIR2=$(BINDIR)");
   }
 }
