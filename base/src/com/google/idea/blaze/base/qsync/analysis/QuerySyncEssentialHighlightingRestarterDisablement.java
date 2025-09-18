3bb68255025a2f24873ab396214d5f30ee38794f
Błażej Kardyś <blazej.kardys@jetbrains.com>
2025-09-18 12:17:09 +0200

    Adding implementation for EssentialHighlightingRestarterDisablement (#7104)

---
+++ base/src/com/google/idea/blaze/base/qsync/analysis/QuerySyncEssentialHighlightingRestarterDisablement.java
@@ -0,0 +1,15 @@
+package com.google.idea.blaze.base.qsync.analysis;
+
+import com.google.idea.blaze.base.settings.Blaze;
+import com.google.idea.blaze.base.settings.BlazeImportSettings;
+import com.intellij.codeInsight.daemon.EssentialHighlightingRestarterDisablement;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+public class QuerySyncEssentialHighlightingRestarterDisablement implements EssentialHighlightingRestarterDisablement {
+
+  @Override
+  public boolean shouldBeDisabledForProject(@NotNull Project project) {
+    return Blaze.getProjectType(project) == BlazeImportSettings.ProjectType.QUERY_SYNC;
+  }
+}
