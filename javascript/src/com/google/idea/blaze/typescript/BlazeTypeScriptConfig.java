e3e02957b8e5d4522cd1a7f1fffb63d9839dc001
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-09-25 17:48:28 +0200

    inlined unnecessary compats

--- javascript/src/com/google/idea/blaze/typescript/BlazeTypeScriptConfig.java
+++ javascript/src/com/google/idea/blaze/typescript/BlazeTypeScriptConfig.java
@@ -29,12 +29,12 @@ import com.google.idea.blaze.base.model.primitives.Label;
 import com.google.idea.blaze.base.model.primitives.WorkspaceRoot;
 import com.google.idea.blaze.base.settings.Blaze;
 import com.google.idea.blaze.base.settings.BuildSystemName;
-import com.google.idea.sdkcompat.javascript.JSModulePathSubstitutionAdapter;
 import com.google.idea.sdkcompat.javascript.TypeScriptConfigAdapter;
 import com.intellij.lang.javascript.config.JSFileImports;
 import com.intellij.lang.javascript.config.JSFileImportsImpl;
 import com.intellij.lang.javascript.config.JSModuleResolution;
 import com.intellij.lang.javascript.config.JSModuleTarget;
+import com.intellij.lang.javascript.frameworks.modules.JSModuleMapping;
 import com.intellij.lang.javascript.frameworks.modules.JSModulePathMappings;
 import com.intellij.lang.javascript.frameworks.modules.JSModulePathSubstitution;
 import com.intellij.lang.javascript.library.JSLibraryUtil;
@@ -737,7 +737,7 @@ class BlazeTypeScriptConfig extends TypeScriptConfigAdapter {
     return false;
   }
 
-  static class PathSubstitution extends JSModulePathSubstitutionAdapter {
+  static class PathSubstitution implements JSModulePathSubstitution {
     private final String pattern;
     private final ImmutableList<String> mappings;
 
@@ -761,8 +761,8 @@ class BlazeTypeScriptConfig extends TypeScriptConfigAdapter {
     }
 
     @Override
-    public Collection<String> getMappingsAsStrings() {
-      return mappings;
+    public Collection<JSModuleMapping> getMappings() {
+      return mappings.stream().distinct().map(JSModuleMapping::new).collect(ImmutableList.toImmutableList());
     }
 
     @Override
