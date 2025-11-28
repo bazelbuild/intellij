914a792650359a20b636f5c7a27afa9ebe1b3042
Błażej Kardyś <blazej.kardys@jetbrains.com>
2025-11-28 15:44:16 +0100

    Updating platform and dependencies versions for 2025.2.5 (#8022)

--- base/tests/unittests/com/google/idea/blaze/base/util/VersionCheckerTest.java
+++ base/tests/unittests/com/google/idea/blaze/base/util/VersionCheckerTest.java
@@ -128,6 +128,16 @@ public class VersionCheckerTest {
     }
 
     @Override
+    public @Nullable String getProductUrl() {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public @Nullable String getJetBrainsTvUrl() {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
     public boolean hasHelp() {
       throw new UnsupportedOperationException();
     }
