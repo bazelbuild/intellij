4ad020c030092b37b2a94fe62d0e0203c1846d66
Daniel Brauner <daniel.brauner@jetbrains.com>
2025-12-15 10:07:57 +0100

    Add RadBazelProjectFallbackConfigDisabler (#8055)
    We found that providing fallback configuration to the backend leads to recursive watch roots being set up for include paths mentioned in the fallback config. We know this has very negative performance implications because of greediness in VfsWarmQueue. This extension point allows us to disable fallback configurations for Bazel projects.
    
    However, since the extension point was introduced in 2025.2.5, it is not save to simply instantiate the implementation class and we need to do some manual registration magic.

---
+++ sdkcompat/v251/com/google/idea/sdkcompat/radler/RadProjectFallbackConfigDisabler.kt
@@ -0,0 +1,11 @@
+package com.google.idea.sdkcompat.radler
+
+import com.intellij.openapi.project.Project
+
+/**
+ * This is a do nothing stub. #api251
+ */
+interface RadProjectFallbackConfigDisabler {
+
+  suspend fun disableFallbackConfigForProject(project: Project): Boolean
+}
